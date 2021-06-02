37e3237b0a0f3c63df5aacdd8fa582885a7738e5
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-02 16:10:10 -0300

    Finalizado requisito obrigatorio 5 e 6 - Adicione um botão com id='criar-tarefa' e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo/Ordene os itens da lista de tarefas por ordem de criação

---
+++ script.js
@@ -0,0 +1,31 @@
+// Funções de comportamento/resposta a eventos
+function addTaskBehavior(event) {
+  const taskInput = document.querySelector('#texto-tarefa');
+  const taskList = document.querySelector('#lista-tarefas');
+
+  if (taskInput.value) {
+    const task = document.createElement('li');
+
+    task.innerHTML = taskInput.value;
+
+    taskList.appendChild(task);
+  }
+
+  taskInput.value = '';
+}
+
+// Waiting page to load
+window.onload = () => {
+  // Event Bubbling
+  document.body.addEventListener('click', (event) => {
+    const targetClass = event.target.classList[0];
+
+    switch (targetClass) {
+      case 'add-task':
+        addTaskBehavior(event);
+        break;
+      default:
+        break;
+    }
+  });
+}
