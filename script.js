4d0e50934575ce0f3c3b3c9c1373706599567bed
PedroSpelta <p.h.s.8989@hotmail.com>
2021-06-02 03:11:46 -0300

    fazendo req 7

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('oi');
+function clickListItem(event) {
+  const { target } = event;
+  target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+function createTaskPress(event) {
+  console.log('teste');
+  const input = document.querySelector('#texto-tarefa');
+  const list = document.querySelector('#lista-tarefas');
+  const listItem = document.createElement('li');
+  if (input.value !== '') {
+    listItem.innerText = input.value;
+    input.value = '';
+    list.appendChild(listItem);
+    list.addEventListener('click', clickListItem);
+  }
+}
