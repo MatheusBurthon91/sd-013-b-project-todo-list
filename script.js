e6a8a4ab862035c4ee44276f6c2d34a5a1a94c64
Guilherme C <guilhermedc93@gmail.com>
2021-06-02 15:36:06 -0300

    concli etapa 7 e 8

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ const olTaskList = document.querySelector('#lista-tarefas');
 
 function addTaskToList(task) {
   const newTask = document.createElement('li');
+  newTask.setAttribute('class', 'task');
   newTask.innerHTML = task;
   olTaskList.appendChild(newTask);
 }
 
-addTaskBtn.addEventListener('click', (e) => {
+addTaskBtn.addEventListener('click', () => {
   if (input.value !== '') {
     addTaskToList(input.value);
     input.value = '';
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('task')) {
+    const tasks = document.querySelector('#lista-tarefas').children;
+    for (let i = 0; i < tasks.length; i += 1) {
+      tasks[i].style.background = 'none';
+    }
+    e.target.style.background = 'rgb(128, 128, 128)';
   }
 });
