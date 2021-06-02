a678d4c24ecbb6bdc5d8638c823c3bf2d4dea87b
marcus <marcus.rodrigues10@hotmail.com>
2021-06-02 14:52:47 -0300

    requirement 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// Requisito 1
+let header = document.getElementsByTagName('header')[0];
+let createTitle = document.createElement('h1');
+header.appendChild(createTitle);
+let title = document.querySelector('h1').innerText = 'Minha Lista de Tarefas';
+
+// Requisito 2
