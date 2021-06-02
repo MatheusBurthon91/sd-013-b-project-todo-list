c2bac7345e840d0a90475eb157aa34e91a045fd8
¨ViniciusCarvalho-Tribo13-B¨ <vinicius.10.carvalho.100@gmail.com>
2021-06-02 18:41:24 -0300

    desafio 8

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const buttonAdd = document.querySelector('#criar-tarefa')
+const buttonAdd = document.querySelector('#criar-tarefa');
 
 buttonAdd.addEventListener('click', () => {
-  const lista = document.querySelector('#lista-tarefas')
-  const input = document.querySelector('#texto-tarefa')
-  const li = document.createElement('li')
+  const lista = document.querySelector('#lista-tarefas');
+  const input = document.querySelector('#texto-tarefa');
+  const li = document.createElement('li');
   lista.appendChild(li);
   li.innerText = input.value;
-  input.value = ''
-})
+  input.value = '';
+});
 
-const list = document.querySelector('#lista-tarefas')
+const list = document.querySelector('#lista-tarefas');
 
+function classRemove() {
+  const classTrue = document.getElementsByClassName('liSelectedColor')[0];
+  if (classTrue) {
+    classTrue.classList.remove('liSelectedColor');
+  }
+}
 list.addEventListener('click', (event) => {
-  let li = document.getElementsByTagName('li')
-  let color = li.className = 'liSelectedColor'
-  event.target.classList.add(color)
-})
+  classRemove();
+  event.target.classList.add('liSelectedColor');
+});
