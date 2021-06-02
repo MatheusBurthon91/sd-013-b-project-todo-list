7becaa1d5c9454c137cc040e0d93368ce5a9f66b
Leonardo <leo_mc_inter@hotmail.com>
2021-06-02 18:31:11 -0300

    Requisitos 6, 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const button = document.querySelector('#criar-tarefa');
 
+function setClass(element) {
+  element.addEventListener('click', () => {
+    const selectedItem = document.querySelector('.selected');
+    if (selectedItem !== null) {
+      element.classList.add('selected');
+      selectedItem.classList.remove('selected');
+    } else {
+      element.classList.add('selected');
+    }
+  });
+}
+
 function setTask() {
   const input = document.querySelector('#texto-tarefa');
   const item = document.createElement('li');
   item.innerText = input.value;
+  item.className = 'item';
   input.value = '';
   const lista = document.querySelector('#lista-tarefas');
   lista.appendChild(item);
+  setClass(item);
 }
 
 button.addEventListener('click', setTask);
