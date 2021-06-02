708ecff6076514484deee310a4984b27762098ef
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-02 16:58:00 -0300

    Requisito 5 - Adicionado os EventListeners e funçoes necessarias. De quebra, completou o Requisito 6 e parte do 7

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// Variables
 const funcionamento = document.getElementById('funcionamento');
 const input = document.getElementById('texto-tarefa');
-const botao = document.getElementById('criar-tarefa');
+const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
+
+// Functions
+function createListItem() {
+  if (input.value === '') {
+    alert('Tarefa vazia');
+  } else {
+    const createLi = document.createElement('li');
+    createLi.innerText = input.value;
+    ol.appendChild(createLi);
+  }
+}
+
+function clearInput() {
+  input.value = '';
+}
+
+button.addEventListener('click', () => {
+  createListItem();
+  clearInput();
+});
