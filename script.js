79569c87b6567da42c695733086ef2424038032b
Alex <alexcorreas1994@gmail.com>
2021-06-02 18:05:36 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function adicionarTarefas() {
 
 const pegarCriarTarefas = document.querySelector('#criar-tarefa');
 pegarCriarTarefas.addEventListener('click', adicionarTarefas);
+
+function oneSelected(event) {
+  const selectedElement = document.querySelector('.selected');
+  selectedElement.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+const pegarListaTarefa = document.querySelector('#lista-tarefas');
+pegarListaTarefa.addEventListener('click', (event) => {
+  const evento = event;
+  evento.target.style.background = 'rgb(128, 128, 128)';
+});
+// pegarListaTarefa.target.style.background = 'rgb(128,128,128)';
