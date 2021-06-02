0310daff278183e30633aa558d26f3e60e75da34
AC-Oliveira <allancardoso21@gmail.com>
2021-06-02 18:55:20 -0300

    All mandatory requirements fulfilled.

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 const taskButton = document.querySelector('#criar-tarefa');
 const addTask = document.querySelector('#texto-tarefa');
+const eraseButton = document.querySelector('#apaga-tudo');
+const eraseFinishedButton = document.querySelector('#remover-finalizados');
 
 // Criação da Função que remove o background de todas as listas.
-function removeBackground(){
-  let listOfLists = document.querySelectorAll('.toDoTask')
-  for( let index1 of listOfLists){
+function removeBackground() {
+  const listOfLists = document.querySelectorAll('.toDoTask');
+  for (const index1 of listOfLists) {
     index1.style.backgroundColor = '';
   }
 }
 
 // Criação da Função que torna o Background do item selecionado cinza.
-function changeBackground (event1){
-  let abc = event1.target.style.backgroundColor;
-    removeBackground();
-    event1.target.style.backgroundColor = 'rgb(128,128,128)'
+function changeBackground(event1) {
+  removeBackground();
+  event1.target.style.backgroundColor = 'rgb(128,128,128)';
 }
-// Cria função responsável por riscar o texto
-function changeText(event2){
-  if (event2.target.classList.length === 1){
-    event2.target.className += ' completed';
-    console.log(event2.target.classList)
-  }else if (event2.target.classList.length === 2) {
-    event2.target.classList.remove('completed');
-    console.log(event2.target.classList)
-  }
+// Cria função responsável por adcionar e remover a classe que risca o texto
+function changeText(event2) {
+  event2.target.classList.toggle('completed');
 }
 
 // Adciona EventListener no butão que ao ser clicado
-function addListItem (){
-  let newTask = document.createElement('li')
-  if (addTask.value != '') {
+function addListItem() {
+  const newTask = document.createElement('li');
+  if (addTask.value !== '') {
     newTask.innerText = addTask.value;
-    document.querySelector('#lista-tarefas').appendChild(newTask)
-    newTask.className = 'toDoTask'; 
+    document.querySelector('#lista-tarefas').appendChild(newTask);
+    newTask.className = 'toDoTask';
     newTask.addEventListener('click', changeBackground);
     newTask.addEventListener('dblclick', changeText);
     addTask.value = '';
-  }  
-}  
-taskButton.addEventListener('click',  addListItem)
+  }
+}
+taskButton.addEventListener('click', addListItem);
+
+// Cria a função que apaga lista
+function eraseList(){ 
+  let listToErase = document.getElementsByClassName('toDoTask');
+  for (let index2 = listToErase.length - 1; index2 >= 0; index2 -= 1) {
+    listToErase[index2].remove();
+  }
+}
+
+// Adciona o event listener ao butão que apaga tudo.
+eraseButton.addEventListener('click', eraseList)
+
+// Cria a função que apaga a lista de tarefas finalizadas.
+function eraseFinishedList(){ 
+  let finishedListToErase = document.getElementsByClassName('completed');
+  for (let index3 = finishedListToErase.length - 1; index3 >= 0; index3 -= 1) {
+    finishedListToErase[index3].remove();
+  }
+}
+
+// Adciona o event listener ao butão que apaga as tarefas finalizadas.
+eraseFinishedButton.addEventListener('click', eraseFinishedList)
+
