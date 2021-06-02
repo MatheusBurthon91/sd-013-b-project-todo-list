738efa24f553101c69a7ad491e912938963711f8
Lopes <gus.lcampos38@gmail.com>
2021-06-02 16:58:49 -0300

    conclui requisito 5, reseta o texto na caixa de entrada

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+//funcao para colocar a skill na lista
+function getText()  {
+  // let texto = document.getElementById('texto-tarefa').value
+  // console.log(texto)
+  let li = document.createElement('li')
+  li.innerText = document.getElementById('texto-tarefa').value
+  let ol = document.getElementById('lista-tarefas')
+  ol.appendChild(li)
+  // console.log(ol)
+  document.getElementById('texto-tarefa').value = '';
+}
+
+
+
+function pegaTexto()  {
+  const adiciona = document.getElementById('criar-tarefa')
+  adiciona.addEventListener('click', getText)
+
+
+  
+}
+
+
+pegaTexto()
