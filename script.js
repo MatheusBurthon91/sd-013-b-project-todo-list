635af50a5a9b2131292c4129c79ff37465efd92b
Lúcio BJ <lbjunq@hotmail.com>
2021-06-02 16:20:49 -0300

    Adicionado o sexto, setimo e oitavo requisitos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const taskInput = document.getElementById('texto-tarefa');
 const list = document.createElement('ol');
-list.id = 'task-list';
+list.id = 'lista-tarefas';
 list.className = 'list';
 document.body.appendChild(list);
 const taskButton = document.createElement('button');;
@@ -9,10 +9,31 @@ taskButton.className = 'button';
 taskButton.innerText = 'Adicionar tarefa';
 document.body.appendChild(taskButton);
 taskButton.addEventListener('click', addTask);
+
 function addTask() {
-  const listItem = document.createElement ('li');
+  const listItem = document.createElement('li');
   listItem.className = 'list-item';
   listItem.innerText = taskInput.value;
-  list.appendChild (listItem);
+  list.appendChild(listItem);
   taskInput.value = '';
+  updateListItens();
+}
+
+const listItens = document.getElementsByClassName('list-item');
+
+function updateListItens() {
+  for (listElements of listItens) {
+    listElements.addEventListener('click', changeBackgrondColor);
+  }
+}
+
+function changeBackgrondColor(event) {
+  for (listElements of listItens) {
+    if (listElements.className.includes ('selected')) {
+      listElements.className = 'list-item';
+      listElements.style.backgroundColor = 'white';
+    }
+  }
+  event.target.className += ' selected';
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
