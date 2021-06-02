b33431f7d8e9b19db22123f17bee907e53ecb844
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-06-02 18:47:41 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-document.getElementById('criar-tarefa').addEventListener('click', addAssigment);
 const ol = document.getElementById('lista-tarefas');
-let TextBox = document.getElementById('texto-tarefa');
-
-function addAssigment(event) {
-  const adicionar = event;
-	let listas = document.createElement('li');
-	listas.className = 'listItem';
-	listas.innerText = TextBox.value;
-	TextBox.value = '';
-	ol.appendChild(listas);
+function addAssigment() {
+  const TextBox = document.getElementById('texto-tarefa');
+  if (TextBox.value !== '') {
+    const listas = document.createElement('li');
+    listas.className = 'listItem';
+    listas.innerText = TextBox.value;
+    TextBox.value = '';
+    ol.appendChild(listas);
+  }
 }
+document.getElementById('criar-tarefa').addEventListener('click', addAssigment);
+function changeColor(event) {
+  const click = event;
+  if (click.target.className === 'listItem') {
+    if (click.target.style.backgroundColor === 'rgb(128, 128, 128)') {
+      click.target.style.backgroundColor = '';
+      console.log(click.target);
+    } else {
+      click.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  }
+}
+document.querySelector('#lista-tarefas').addEventListener('click', changeColor);
