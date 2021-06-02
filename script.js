d2d27b3561036d5b98925b7bbba86bde252226ec
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-02 17:17:54 -0400

    vamo de 7

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-window.onload = function() {
+window.onload = function () {
   const button = document.getElementById('criar-tarefa');
   const tarefa = document.getElementById('texto-tarefa');
   const lista = document.getElementById('lista-tarefas');
 
-  button.addEventListener('click', function() {
-    let novaTarefa = tarefa.value;
+  function addTarefa() {
+    const novaTarefa = tarefa.value;
     tarefa.value = '';
-    let listaTarefa = document.createElement('li');
-    listaTarefa.innerText = novaTarefa
+    const listaTarefa = document.createElement('li');
+    listaTarefa.innerText = novaTarefa;
+    listaTarefa.className = 'itemLista';
     lista.appendChild(listaTarefa);
-  })
-}
+  };
+
+  button.addEventListener('click', addTarefa);
+  
+  function trocaCorFundo(event) {
+    let itemClicado = event.target;
+    itemClicado.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+
+  lista.addEventListener('click', trocaCorFundo);
+};
