4fc78902f45c4dece4ff1f38e88017ed4113c4f8
Lucas Caribé <lucasmoc47@gmail.com>
2021-06-02 16:51:44 -0300

    Requisitos 11 e 12

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
 const taskListId = '#lista-tarefas';
 
-function createNewTask(text, id) {
+function createNewTask(text, classList = 'task') {
   const task = document.createElement('li');
   task.innerText = text;
-  task.id = id;
-  task.className = 'task';
+  task.classList = classList;
 
   return task;
 }
 
-function addButtonEventListener() {
+function classObjectToString(obj) {
+  const array = Object.entries(obj).map((value) => `${value[1]} `);
+
+  return array.toString().replace(/,/g, '');
+}
+
+function saveToLocalStorage(tasksArray) {
+  localStorage.setItem('tasks', JSON.stringify(tasksArray));
+}
+
+function retrieveFromLocalStorage() {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+  const taskList = document.querySelector(taskListId);
+
+  if (storedTasks) {
+    storedTasks.forEach(({ text, classList }) => {
+      const classListString = classObjectToString(classList);
+
+      taskList.appendChild(createNewTask(text, classListString));
+    });
+  }
+}
+
+function addTaskButtonEventListener() {
   const addTaskButton = document.querySelector('#criar-tarefa');
 
   addTaskButton.addEventListener('click', (event) => {
     event.preventDefault();
-
     const textInput = document.querySelector('#texto-tarefa');
 
     if (textInput.value) {
       const taskList = document.querySelector(taskListId);
-      const taskNumber = taskList.children.length;
 
-      taskList.appendChild(createNewTask(textInput.value, `task${taskNumber}`));
+      taskList.appendChild(createNewTask(textInput.value));
       textInput.value = '';
     } else {
       alert('Sua tarefa precisa ter um texto descritivo!');
     }
+  });
+}
+
+function addInputEventListener() {
+  const textInput = document.querySelector('#texto-tarefa');
+  const addTaskButton = document.querySelector('#criar-tarefa');
+
+  textInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') addTaskButton.click();
   });
 }
 
@@ -76,9 +105,45 @@ function addClearListEvent() {
   });
 }
 
+function addClearCompletedEvent() {
+  const clearCompletedButton = document.querySelector('#remover-finalizados');
+
+  clearCompletedButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const completedTasks = document.querySelectorAll('.completed');
+
+    completedTasks.forEach((task) => task.remove());
+  });
+}
+
+function addSaveTasksEvent() {
+  const saveTasksButton = document.querySelector('#salvar-tarefas');
+
+  saveTasksButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const tasks = document.querySelectorAll('.task');
+    const tasksObjectArray = [];
+
+    tasks.forEach((task) => {
+      tasksObjectArray.push({
+        text: task.innerText,
+        classList: task.classList,
+      });
+    });
+
+    saveToLocalStorage(tasksObjectArray);
+  });
+}
+
 window.onload = () => {
-  addButtonEventListener();
+  retrieveFromLocalStorage();
+  addTaskButtonEventListener();
+  addInputEventListener();
   addTaskSelectionEvent();
   addTaskCompletionEvent();
   addClearListEvent();
+  addClearCompletedEvent();
+  addSaveTasksEvent();
 };
