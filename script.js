e5165fa3b76df136bc791b1bf070d9ea96c82005
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-06-02 18:45:46 -0300

    Exercise 3 and 4

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const h1 = document.createElement('h1');
 
-const HeaderSelect = document.querySelector('header').appendChild(h1);
-
 const divFunction = document.body.children[1];
 
 const inputSelect = document.querySelector('input');
 
 const olSelect = document.querySelector('ol');
 
+const funcId = document.getElementById('funcionamento')
+document.querySelector('header').appendChild(h1);
+
 h1.innerText = 'Minha Lista de Tarefas';
 
 divFunction.setAttribute('id', 'funcionamento');
 
-document.getElementById('funcionamento').innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+funcId.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 
 inputSelect.setAttribute('id', 'texto-tarefa');
 
