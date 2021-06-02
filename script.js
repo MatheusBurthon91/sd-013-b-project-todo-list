3c9a5c77031ca26346d9b7f343cd87af44c6dc52
Rafael Romano <rafa-kun@hotmail.com>
2021-06-02 18:32:56 -0300

    adicionando exercicios ate o 8

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ const listaTarefa = document.querySelector('#lista-tarefas');
 function adicionarTarefa() {
   let input = textTarefa.value;
   let li = document.createElement('li');
-  listaTarefa.appendChild(li);
   li.innerText = input;
   textTarefa.value = '';
+  listaTarefa.appendChild(li);
 }
 
 botaoAdi.addEventListener('click', adicionarTarefa);
+  
+listaTarefa.addEventListener('click',(event)=>{
+  removendoClasse()
+  
+       event.target.classList.add('selected')
+
+})
+//Source=Exercicio 8 inspirado na resolução do Ygor Lages link:https://github.com/tryber/sd-013-b-project-todo-list/tree/ygor-project-todo-list
+
+function removendoClasse(evento){
+  //seleciona o elemento com a classe selected na posição 0
+  let li=document.getElementsByClassName('selected')[0];
+  //se caso ele achar um li na posição 0 ele vai exclui-lo
+  
+  if(li){  
+    li.classList.remove('selected')
+  }
+}
