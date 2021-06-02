709a9b37808e726078ce9d720eec00869931c9b9
Carol_Inglethe <carolinainglethe@gmail.com>
2021-06-02 16:15:58 -0300

    Adicionando texto no botao criar tarefas

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const botaoCriarTarefa = document.querySelector('#criar-tarefa');
 const caixaDeInput = document.querySelector('#texto-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
 
+// Requesito 5 && 6 - evento click no botao e entrega valor de input na lista
 function criaItemLista() {
   const itemLista = document.createElement('li');
   itemLista.innerText = caixaDeInput.value;
@@ -9,3 +10,4 @@ function criaItemLista() {
   caixaDeInput.value = '';
 }
 botaoCriarTarefa.addEventListener('click', criaItemLista);
+botaoCriarTarefa.innerText = 'Criar Tarefa';
