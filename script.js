73ea08bd25746758525d0227dd1fae51315b2dfb
Yg-or <inygu@icloud.com>
2021-06-02 15:20:23 -0300

    requisitos 5 e 6

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ function criaLista() {
   pai.appendChild(lista)
 }
 criaLista();
+
+
+function adicionaBotao() {
+  const pai = document.getElementById('inputArea')
+  const btn = document.createElement('button')
+  btn.id = "criar-tarefa"
+  btn.addEventListener('click', adicionaTarefa)
+  btn.textContent = "Adicionar"
+  btn.className = "btnAdicionar"
+  pai.appendChild(btn)
+}
+
+adicionaBotao();
+
+
+function adicionaTarefa() {
+  const lista = document.getElementById('lista-tarefas')
+  const item = document.createElement('li')
+  item.textContent = confereCampo()
+  lista.appendChild(item)
+  
+  
+}
+
+
+function confereCampo() {
+  const campo = document.getElementById('texto-tarefa')
+  let temp = campo.value
+  if(campo.value){
+    campo.value = ""
+    return temp
+  }
+
+}
