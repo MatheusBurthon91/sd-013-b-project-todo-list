1c51b6ff6bf10799eb3b599d441613ed9f1493b2
Lucas Caribé <lucasmoc47@gmail.com>
2021-06-02 15:51:53 -0300

    Requisitos 7, 8, 9 e 10

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const taskListId = '#lista-tarefas';
+
 function createNewTask(text, id) {
   const task = document.createElement('li');
   task.innerText = text;
   task.id = id;
+  task.className = 'task';
 
   return task;
 }
@@ -15,11 +18,10 @@ function addButtonEventListener() {
     const textInput = document.querySelector('#texto-tarefa');
 
     if (textInput.value) {
-      const taskList = document.querySelector('#lista-tarefas');
+      const taskList = document.querySelector(taskListId);
+      const taskNumber = taskList.children.length;
 
-      taskList.appendChild(
-        createNewTask(textInput.value, `task${taskList.children.length}`),
-      );
+      taskList.appendChild(createNewTask(textInput.value, `task${taskNumber}`));
       textInput.value = '';
     } else {
       alert('Sua tarefa precisa ter um texto descritivo!');
@@ -27,6 +29,56 @@ function addButtonEventListener() {
   });
 }
 
+function addTaskSelectionEvent() {
+  const taskList = document.querySelector(taskListId);
+
+  taskList.addEventListener('click', (event) => {
+    const { target } = event;
+
+    if (target.classList.contains('task')) {
+      const currentSelected = document.querySelector('.selected');
+
+      if (currentSelected) {
+        currentSelected.classList.remove('selected');
+        target.classList.add('selected');
+      } else {
+        target.classList.add('selected');
+      }
+    }
+  });
+}
+
+function addTaskCompletionEvent() {
+  const taskList = document.querySelector(taskListId);
+
+  taskList.addEventListener('dblclick', (event) => {
+    const { target } = event;
+
+    if (target.classList.contains('task')) {
+      if (target.classList.contains('completed')) {
+        target.classList.remove('completed');
+      } else {
+        target.classList.add('completed');
+      }
+    }
+  });
+}
+
+function addClearListEvent() {
+  const clearButton = document.querySelector('#apaga-tudo');
+
+  clearButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const taskList = document.querySelector(taskListId);
+
+    taskList.innerHTML = '';
+  });
+}
+
 window.onload = () => {
   addButtonEventListener();
+  addTaskSelectionEvent();
+  addTaskCompletionEvent();
+  addClearListEvent();
 };
