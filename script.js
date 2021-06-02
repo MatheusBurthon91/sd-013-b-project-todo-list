8c55ffa1846910f6189c0d1b6b2a5a16ed6acc47
Henrique Cursino <henriquecursino46@gmail.com>
2021-06-02 18:25:06 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function adicionar() {
     const text = document.getElementById('texto-tarefa').value;
     const list = document.getElementById('lista-tarefas');
     const newEl = document.createElement('li');
+    document.querySelector('#texto-tarefa').value = null;
     newEl.innerText = text;
     list.appendChild(newEl);
 }
