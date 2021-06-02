9961ca070e5e662b844480a574169eb6fe63ce0f
Felipe Ventorim <felipeventorim11@gmail.com>
2021-06-02 15:13:04 -0300

    Requisito 7 - click no item da lista muda o background para cinza

--- script.js
+++ script.js
@@ -11,9 +11,17 @@ function addTask() {
   ol.appendChild(createLi());
 }
 
+function backgroundColorItem(event) {
+  const eventTarget = event.target;
+  eventTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function start() {
   const button = document.getElementById('criar-tarefa');
   button.addEventListener('click', addTask);
+
+  const listItem = document.getElementById('lista-tarefas');
+  listItem.addEventListener('click', backgroundColorItem);
 }
 
 window.onload = start;
