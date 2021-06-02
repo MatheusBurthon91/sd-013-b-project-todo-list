7207ea051a81a528f96614b011531bb1bcd27d13
PedroSpelta <p.h.s.8989@hotmail.com>
2021-06-02 10:40:39 -0300

    fazendo req 8 e 9

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+function removeSelectedListItem() {
+  const list = document.querySelectorAll('li');
+  for (let index = 0; index < list.length; index += 1) {
+    if (list[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+      list[index].style.backgroundColor = 'white';
+      return;
+    }
+  }
+}
 function clickListItem(event) {
   const { target } = event;
+  removeSelectedListItem();
   target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
+function markListItem(event) {
+  const { target } = event;
+  const decoration = target.style.textDecoration;
+  if (target.classList < 1) target.classList.add('completed');
+  else target.classList.remove('completed');
+  console.log(decoration);
+}
 function createTaskPress(event) {
-  console.log('teste');
   const input = document.querySelector('#texto-tarefa');
   const list = document.querySelector('#lista-tarefas');
   const listItem = document.createElement('li');
@@ -12,5 +28,6 @@ function createTaskPress(event) {
     input.value = '';
     list.appendChild(listItem);
     list.addEventListener('click', clickListItem);
+    list.addEventListener('dblclick', markListItem);
   }
 }
