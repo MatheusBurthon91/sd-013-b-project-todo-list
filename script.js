7418d08a78bbc0a4eb95bc9f836d4445cffa1b4e
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-06-02 15:09:57 -0300

    Requisito 5 Concluido

---
+++ script.js
@@ -0,0 +1,22 @@
+function createItemList(value) {
+  const orderList = document.getElementById('lista-tarefas');
+  const element = document.createElement('li');
+  element.innerText = value;
+  orderList.appendChild(element);
+}
+
+function createTask() {
+  const input = document.getElementById('texto-tarefa');
+  createItemList(input.value);
+  input.value = '';
+}
+
+function init() {
+  const buttonAddTask = document.getElementById('criar-tarefa');
+
+  buttonAddTask.addEventListener('click', createTask);
+}
+
+window.onload = () => {
+  init();
+};
