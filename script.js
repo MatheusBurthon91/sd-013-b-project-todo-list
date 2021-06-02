1a843f763abd86bc42a808434b0ba2f1f801daf0
Marcos Estevao <dusdoom@gmail.com>
2021-06-02 17:21:18 -0300

    Requisitos 9, 10 & 11.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector('#criar-tarefa').addEventListener('click', () => {
-  const input = document.querySelector('#texto-tarefa');
-  const taskList = document.querySelector('#lista-tarefas');
-  // criar uma li
+document.getElementById('criar-tarefa').addEventListener('click', () => {
+  const input = document.getElementById('texto-tarefa');
+  const taskList = document.getElementById('lista-tarefas');
+  //  uma li
   const newTask = document.createElement('li');
   newTask.classList.add('list-item');
   // colocar o texto do input na li
@@ -11,7 +11,6 @@ document.querySelector('#criar-tarefa').addEventListener('click', () => {
   // resetar o texto
   input.value = '';
 });
-
 document.addEventListener('click', (listItem) => {
   const itemClicked = listItem.target;
   const otherItems = document.getElementsByClassName('list-item');
@@ -20,5 +19,26 @@ document.addEventListener('click', (listItem) => {
       otherItems[index].style.backgroundColor = 'white';
     }
     itemClicked.style.backgroundColor = 'grey';
+  }
+});
+document.addEventListener('dblclick', (listItem) => {
+  const itemClicked = listItem.target;
+  if (!itemClicked.classList.contains('completed') && itemClicked.classList.contains('list-item')) {
+    itemClicked.classList.add('completed');
+  } else if (itemClicked.classList.contains('list-item')) {
+    itemClicked.classList.remove('completed');
+  }
+});
+document.getElementById('apaga-tudo').addEventListener('click', () => {
+  const list = document.getElementById('lista-tarefas');
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+});
+document.getElementById('remover-finalizados').addEventListener('click', () => {
+  const completedItems = document.getElementsByClassName('completed');
+  console.log(completedItems);
+  while (completedItems.length !== 0) {
+    completedItems[0].remove();
   }
 });
