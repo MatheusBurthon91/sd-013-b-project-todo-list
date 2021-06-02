806118b9d631ed5141d7da81ba6535461ab1f041
Joao Victor <joaogmavictor@gmail.com>
2021-06-02 18:04:43 -0300

    Concluindo Requisitos 9

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
-window.onload = function(){
+window.onload = function () {
   const btnOk = document.getElementById('criar-tarefa')
   const olTasks = document.getElementById('lista-tarefas')
   btnOk.addEventListener('click', createTasks)
   olTasks.addEventListener('click', changeBackGround)
+  olTasks.addEventListener('dblclick', completeTasks)
+
 }
 
-function createTasks(){
+function completeTasks(event) {
+  const elementoAlvo = event.target
+  let classList = elementoAlvo.className
+  const completed = elementoAlvo.className
+  let seaparado = completed.split(' ')
+
+  if (seaparado.length > 2) {
+    for (let i = 0; i < seaparado.length; i += 1) {
+      if (seaparado[i] === 'completed') {
+        seaparado.splice(i, 1)
+        elementoAlvo.className = seaparado.join(' ')
+        console.log(completed[i]);
+      }
+    }
+
+  } else {
+    elementoAlvo.className = classList.replace(' completed', '')
+    elementoAlvo.className += ' completed'
+  }
+
+
+}
+
+
+function createTasks() {
   const ol = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
   let conteudo = document.getElementById('texto-tarefa')
   li.innerText = conteudo.value
   conteudo.value = '';
   ol.appendChild(li)
-  console.log('teste');
 
 }
 
-function changeBackGround(event){
-  const elementoAlvo = event.target
-  let classesSelects = document.getElementsByClassName('select')
-  for (let index = 0; index < classesSelects.length; index += 1){
-    classesSelects[index].className = '';
+function changeBackGround(event) {
+  const elementoAlvo = event.target;
+  let classList = elementoAlvo.className;
+  const select = document.getElementsByClassName('select');
+
+
+  for (let i = 0; i < select.length; i += 1) {
+    let clas = select[i].className
+    clas = clas.replace(' select', '')
+    select[i].className = clas
   }
-  elementoAlvo.className = 'select'
-console.log(elementoAlvo);
+
+
+  elementoAlvo.className = classList.replace(' select', '');
+
+  elementoAlvo.className += ' select'
+
+
+
 }
