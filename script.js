32b7b97c273622e800436decc5bded759076e29b
Tenari <guilherme.tenarie@gmail.com>
2021-06-02 16:37:29 -0300

    Adiciona requisito 6 (implicito no 5), 7 e 8

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ function inputTask() {
   const texto = document.getElementById('texto-tarefa').value;
   listItem.innerText = texto;
   orderedList.appendChild(listItem);
+  listItem.style.backgroundColor = 'white';
   document.getElementById('texto-tarefa').value = '';
 }
 
 document.getElementById('criar-tarefa').addEventListener('click', inputTask);
+
+//  Altera a cor dos item da lista ao clicar
+function changeColor(event) {
+  const selectedOne = document.querySelector('.selected');
+  if (selectedOne != null) {
+    selectedOne.style.backgroundColor = 'white';
+    selectedOne.classList.remove('selected');
+  }
+  const listItem = event.target;
+  listItem.style.backgroundColor = 'rgb(128,128,128)'
+  listItem.classList.add('selected');
+}
+
+orderedList.addEventListener('click', changeColor);
