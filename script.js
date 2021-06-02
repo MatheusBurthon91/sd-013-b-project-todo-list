65963fd5d43983e5a00ed254d50f21d9efa6196e
Alex <alexcorreas1994@gmail.com>
2021-06-02 17:18:41 -0300

    Requisito 5 e 6 ok

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-// function adicionarTarefas() {
-//   const pegarTextoTarefas = document.querySelector('#texto-tarefa');
-//   const pegarCriarTarefas = document.querySelector('#criar-tarefa');
-//   const pegarListaTarefa = document.querySelector('#lista-tarefas');
+function adicionarTarefas() {
+  const pegarTextoTarefas = document.querySelector('#texto-tarefa');
+  const pegarListaTarefa = document.querySelector('#lista-tarefas');
 
-//   pegarCriarTarefas.addEventListener ('click', function () {
-//     if (pegarTextoTarefas.value.length > 0) {
-//       const novaLista = document.createElement('li');
-//       novaLista.innerText = pegarTextoTarefas.value;
-//       pegarListaTarefa.appendChild(novaLista);
-//       pegarTextoTarefas.value = '';
-//     } else {
-//       alert('Error: Digite ao menos 1 caractere');
-//     }
-//   })
-// };
+  const novaLista = document.createElement('li');
+  novaLista.innerText = pegarTextoTarefas.value;
+  pegarListaTarefa.appendChild(novaLista);
+  pegarTextoTarefas.value = '';
+}
 
-// adicionarTarefas();
+const pegarCriarTarefas = document.querySelector('#criar-tarefa');
+pegarCriarTarefas.addEventListener('click', adicionarTarefas);
