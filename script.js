50404b3fdf7b307c3b6a70f2211958f051b7855b
FilipeGuto <filipeguto95@gmail.com>
2021-06-02 18:38:57 -0300

    Exercise 7

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // elemento criado com o texto do input;
 // clicar no botão cria um elemento;
 
+// clicar item altera background
+function color(liItem) {
+  // const liItem = document.querySelectorAll('li');
+  // for (let index = 0; index < liItem.length; index += 1) {
+  liItem.addEventListener('click', () => {
+    liItem.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+}
 function element() {
   const text = document.querySelector('#texto-tarefa').value;
   const list = document.querySelector('#lista-tarefas');
   const newElement = document.createElement('li');
+  color(newElement);
   document.querySelector('#texto-tarefa').value = null;
   newElement.innerText = text;
   list.appendChild(newElement);
@@ -12,4 +21,3 @@ function element() {
 
 const button = document.querySelector('#criar-tarefa');
 button.addEventListener('click', element);
-// elemento adicionado, sai do input;
