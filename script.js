8c6aa0094eebdb32204b6794ad5c7a07d702dffd
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-02 16:46:57 -0300

    adiciona as primeiras variaveis no script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const funcionamento = document.getElementById('funcionamento');
+const input = document.getElementById('texto-tarefa');
+const botao = document.getElementById('criar-tarefa');
+const ol = document.getElementById('lista-tarefas');
