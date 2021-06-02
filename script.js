5385385f8806be12b4d1662af53505209b20c150
Lúcio BJ <lbjunq@hotmail.com>
2021-06-02 15:04:23 -0300

    Adicionado o terceiro e quarto requisito

---
+++ script.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById('texto-tarefa');
+const list = document.createElement('ol');
+list.className = 'list'
+list.id = 'task-list'
+document.body.appendChild(list);
