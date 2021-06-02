4b4fa9213009c703e65e2f9ead17f6e4cd3396e8
Lúcio BJ <lbjunq@hotmail.com>
2021-06-02 15:15:12 -0300

    Adicionado o quinto requisito

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const taskInput = document.getElementById('texto-tarefa');
 const list = document.createElement('ol');
-list.className = 'list'
-list.id = 'task-list'
+list.id = 'task-list';
+list.className = 'list';
 document.body.appendChild(list);
+const taskButton = document.createElement('button');;
+taskButton.id = 'criar-tarefa';
+taskButton.className = 'button';
+taskButton.innerText = 'Adicionar tarefa';
+document.body.appendChild(taskButton);
+taskButton.addEventListener('click', addTask);
+function addTask() {
+  const listItem = document.createElement ('li');
+  listItem.className = 'list-item';
+  listItem.innerText = taskInput.value;
+  list.appendChild (listItem);
+  taskInput.value = '';
+}
