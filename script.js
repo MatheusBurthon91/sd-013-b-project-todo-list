d1f38c5096a616885da6d4fd54138001355822f2
Alessandro Hugen <alessandrohugen@gmail.com>
2021-06-02 18:31:13 -0300

    Finzaliza os Obrigatorios

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //5
-document.body.backgroundColor = 'teal';
 let taskList = document.getElementById('lista-tarefas');
 let addButton = document.getElementById('criar-tarefa');
 let clearButton =  document.getElementById('apaga-tudo');
+let completedButton = document.getElementById('remover-finalizados');
 
 function addTask() {
     let content = document.getElementById('texto-tarefa');
@@ -21,7 +21,7 @@ function changeBGColor(event) {
     for (let index = 0; index < listItemsArray.length; index += 1) {
       if (listItemsArray[index].classList.contains('selected')) {
         listItemsArray[index].classList.remove('selected');
-        listItemsArray[index].style.backgroundColor = 'white';
+        listItemsArray[index].style.backgroundColor = 'teal';
     } else {
       event.target.classList.add('selected');
       event.target.style.backgroundColor = 'rgb(128, 128, 128)';
@@ -32,7 +32,7 @@ function changeBGColor(event) {
 taskList.addEventListener('click', changeBGColor);
 
 function crossItem(event) {
-  if(event.target.classList.contains('completed')) {
+  if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
     event.target.style.textDecoration = 'none';
   } else {
@@ -48,3 +48,12 @@ function clearList() {
 }
 
 clearButton.addEventListener('click', clearList);
+
+function removeCompleted() {
+ const completedTask = document.querySelectorAll('.completed');
+ for (let task of completedTask) {
+     task.remove();
+ }
+}
+
+completedButton.addEventListener('click', removeCompleted);
