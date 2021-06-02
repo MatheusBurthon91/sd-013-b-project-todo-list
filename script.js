9658581bfdaa5ab5889e671af44fa5d6598e30d1
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-06-02 15:11:35 -0300

    Adiciona requisitos 1 ao 5

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 window.onload = function() {
+  heroTitle();
+  explain();
+  input();
+  activityList();
+  createButton();
 };
+
+function heroTitle() {
+  const title = document.createElement('h1');
+  title.innerText = 'Minha Lista de Tarefas';
+  document.querySelector('header').appendChild(title);
+}
+
+function explain() {
+  const text = document.createElement('p');
+  text.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+  text.id = 'funcionamento';
+  document.querySelector('header').appendChild(text);
+}
+
+function input() {
+  const box = document.createElement('input');
+  box.id = 'texto-tarefa';
+  document.querySelector('main').appendChild(box);
+}
+
+function activityList() {
+  const list = document.createElement('ol');
+  list.id = 'lista-tarefas';
+  document.querySelector('main').appendChild(list);
+}
+
+function createButton() {
+  const button = document.createElement('button');
+  button.id = 'criar-tarefa';
+  button.innerText = 'Adicionar';
+  document.querySelector('main').appendChild(button);
+  addToList();
+}
+
+function addToList() {
+  document.querySelector('#criar-tarefa').addEventListener ('click', () => {
+  const inputText = document.querySelector('#texto-tarefa').value;
+  const newItem = document.createElement('li');
+  newItem.innerText = inputText;
+  document.querySelector('ol').appendChild(newItem);
+  document.querySelector('#texto-tarefa').value = null;
+  });
+}
