0ca21c804cc65260294fa74c70711b7a36712d9e
k4nl <gustavoh.bh@gmail.com>
2021-06-02 20:02:44 -0300

    resoluçao exercicio 5, crie um passo a passo de como fiz a resoluçao

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function createElement(text, id) {
-  const element = document.createElement('li');
-  element.innerText = text;
-  element.id = id;
-  element.className = 'tarefa';
-  return element;
+function createLi() {
+  const li = document.createElement('li');
+  const ol = document.querySelector('#lista-tarefas');
+  const input = document.querySelector('#texto-tarefa');
+  li.innerText = input.value;
+  li.className = 'tarefa';
+  ol.appendChild(li);
+  input.value = '';
+}
+
+function addEventListenerButton() {
+  const addElementButton = document.getElementById('criar-tarefa');
+  addElementButton.addEventListener('click', createLi);
 }
 
 window.onload = () => {
-  createOrderedList();
+  addEventListenerButton();
 };
