c7feee89759dd426376ec4670b93aa7f6113c047
eaeniko <eaenikolai@gmail.com>
2021-06-02 18:35:18 -0300

    [Nikolai] Requeriment 7

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const createTaskBtn = document.getElementById('criar-tarefa');
+const taskListOl = document.getElementById('lista-tarefas');
 
 function createTask () {
+
   const inputText = document.getElementById('texto-tarefa');
-  const taskListOl = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
 
+  li.addEventListener('click', selectTaskLi);
+
+  li.className = 'li-task';
   li.innerText = inputText.value;
   taskListOl.appendChild(li);
   inputText.value = '';
-  console.log(inputText);
+
+}
+
+const getTaskLi = document.querySelectorAll('.li-task');
+
+function selectTaskLi(event) {
+  const eventTarget = event.target
+  console.log(eventTarget);
+
+  if (eventTarget.style.backgroundColor === 'rgb(128, 128, 128)') {
+    eventTarget.style.backgroundColor = '';
+  } else {
+
+    eventTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+
+  
 }
 
 window.onload = function () {
