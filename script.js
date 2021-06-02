64914eecb86eaf8573c2a8f5b9d9518f722a95a4
Rafael Friederick <rafael.mf.documents@gmail.com>
2021-06-02 16:42:44 -0300

    Desafio 8 e 9

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function inputTaskinList() {
+function inputTaskList() {
   const inputTaskValue = document.getElementById('texto-tarefa').value;
   if (inputTaskValue) {
     const todoList = document.getElementById('lista-tarefas');
     const listItem = document.createElement('li');
     listItem.innerText = inputTaskValue;
+    listItem.addEventListener('click', () => {
+      for (item of todoList.children) {
+        if (item.style.backgroundColor === 'rgb(128, 128, 128)') {
+          item.style.backgroundColor = 'white';
+        }
+      }
+      listItem.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+    listItem.addEventListener('dblclick', () => {
+      if (listItem.className.includes('completed')) {
+        listItem.classList.remove('completed');
+      } else {
+        listItem.classList.add('completed');
+      }
+    });
     todoList.appendChild(listItem);
   }
   document.getElementById('texto-tarefa').value = '';
@@ -11,5 +26,5 @@ function inputTaskinList() {
 
 window.onload = () => {
   const createTaskButton = document.getElementById('criar-tarefa');
-  createTaskButton.addEventListener('click', inputTaskinList);
+  createTaskButton.addEventListener('click', inputTaskList);
 };
