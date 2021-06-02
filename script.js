1f3be72877e5429bfa1e1f46eb02de70aec49019
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-02 17:27:25 -0300

    Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // Usei o exercicio da piramide do bloco 5 como referencia para o window.onload
-window.onload = function() {
-  let botao = document.getElementById('criar-tarefa');
-  let input = document.getElementById('texto-tarefa')
-  let listaTarefas = document.getElementById('lista-tarefas');
+const botao = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
 
-  function criaTarefa(event) {
-    let tarefa = document.createElement('li');
-    // Referencia da propriedade value https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onkeyup_addeventlistener
-    tarefa.innerText = input.value;
-    listaTarefas.appendChild(tarefa);
-    input.value = "";
-  }
-  botao.addEventListener('click', criaTarefa);
+function criaTarefa() {
+  const tarefa = document.createElement('li');
+  tarefa.className = 'tarefa';
+  // Referencia da propriedade value https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onkeyup_addeventlistener
+  tarefa.innerText = input.value;
+  listaTarefas.appendChild(tarefa);
+  input.value = '';
 }
+botao.addEventListener('click', criaTarefa);
+
+function pintaTarefa(event) {
+  var x = event.target;
+  x.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+function pintar() {
+    listaTarefas.addEventListener('click', pintaTarefa);
+}
+pintar();
