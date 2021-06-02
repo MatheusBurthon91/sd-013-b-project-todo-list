16827e79aa9b0aad02153840d80d281c37da5e2c
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-06-02 18:54:36 -0300

    Exercise 6 and fixes

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const h1 = document.createElement('h1');
 
-const divFunction = document.body.children[1];
-
-const inputSelect = document.querySelector('input');
-
-const olSelect = document.querySelector('ol');
-
-const funcId = document.getElementById('funcionamento')
 document.querySelector('header').appendChild(h1);
 
-h1.innerText = 'Minha Lista de Tarefas';
+const divFunction = document.body.children[1];
 
 divFunction.setAttribute('id', 'funcionamento');
 
-funcId.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+const inputSelect = document.querySelector('input');
 
 inputSelect.setAttribute('id', 'texto-tarefa');
 
+const olSelect = document.querySelector('ol');
+
+const funcId = document.getElementById('funcionamento');
+
+const buttonSelect = document.querySelector('button');
+
+h1.innerText = 'Minha Lista de Tarefas';
+
+funcId.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
 olSelect.setAttribute('id', 'lista-tarefas');
+
+buttonSelect.setAttribute('id', 'criar-tarefa');
