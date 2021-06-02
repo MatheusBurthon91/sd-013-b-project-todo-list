5e00cd44350b2af63f76cdd0471c8ee4f62a49fd
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-02 17:59:54 -0300

    requisito 6, 7 e 8

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ function addTarefa() {
   listaTarefas.appendChild(item);
   inp.value = '';
 }
-
 const botaoAdd = document.getElementById('criar-tarefa');
 botaoAdd.addEventListener('click', addTarefa);
+
+function select(event) {
+  const tarefas = document.getElementsByTagName('li');
+  for (let index = 0; index < tarefas.length; index += 1) {
+    const tarefa = tarefas[index];
+    tarefa.className = '';
+  }
+  event.target.className = 'selected';
+}
+const listaTarefas = document.getElementById('lista-tarefas');
+listaTarefas.addEventListener('click', select);
