8a956a822723a56b4fdbb4b6f27d3df1e1f25b10
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-06-02 17:51:38 -0300

    Requisito 1 funcioanando

---
+++ script.js
@@ -0,0 +1,7 @@
+// Requisito 1
+let header = document.createElement('header');
+document.body.appendChild(header);
+
+let title = document.createElement('h1');
+title.innerText = 'Minha Lista de Tarefas';
+header.appendChild(title);
