4fb68e03c4fd1aad4af111db85e5564a3ec1ccdd
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-06-02 18:58:28 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -11,11 +11,10 @@ function addAssigment() {
 }
 document.getElementById('criar-tarefa').addEventListener('click', addAssigment);
 function changeColor(event) {
-  const click = event; 
+  const click = event;
   if (click.target.className === 'listItem') {
     if (click.target.style.backgroundColor === 'rgb(128, 128, 128)') {
       click.target.style.backgroundColor = '';
-      console.log(click.target);
     } else {
       click.target.style.backgroundColor = 'rgb(128, 128, 128)';
     }
