6911a5752fb19b505b3f914cb4950d598fe5e904
Alex <alexcorreas1994@gmail.com>
2021-06-02 16:51:58 -0300

    fazendo o requisito 5

---
+++ script.js
@@ -0,0 +1,18 @@
+// function adicionarTarefas() {
+//   const pegarTextoTarefas = document.querySelector('#texto-tarefa');
+//   const pegarCriarTarefas = document.querySelector('#criar-tarefa');
+//   const pegarListaTarefa = document.querySelector('#lista-tarefas');
+
+//   pegarCriarTarefas.addEventListener ('click', function () {
+//     if (pegarTextoTarefas.value.length > 0) {
+//       const novaLista = document.createElement('li');
+//       novaLista.innerText = pegarTextoTarefas.value;
+//       pegarListaTarefa.appendChild(novaLista);
+//       pegarTextoTarefas.value = '';
+//     } else {
+//       alert('Error: Digite ao menos 1 caractere');
+//     }
+//   })
+// };
+
+// adicionarTarefas();
