7b9708fc9614dc40b121a2765e29d404dda2799a
Jessica Naiara <jessicanaiarag@gmail.com>
2021-06-02 18:42:31 -0300

    requisito 9

--- script.js
+++ script.js
@@ -14,9 +14,19 @@ function select(event) {
   const tarefas = document.getElementsByTagName('li');
   for (let index = 0; index < tarefas.length; index += 1) {
     const tarefa = tarefas[index];
-    tarefa.className = '';
+    tarefa.className = tarefa.className.replace('selected', '');
   }
-  event.target.className = 'selected';
+  event.target.className += ' selected';
 }
 const listaTarefas = document.getElementById('lista-tarefas');
 listaTarefas.addEventListener('click', select);
+
+listaTarefas.addEventListener('dblclick', risco);
+
+function risco(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.className = event.target.className.replace('completed', '');
+  } else {
+    event.target.className += ' completed';
+  }
+}
