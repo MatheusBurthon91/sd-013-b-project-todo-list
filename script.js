4c81f106f8e4d99ce1c1ee8426e481bcff3a04c2
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-06-02 17:59:35 -0300

    Requisito 2 funcionando

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.body.appendChild(header);
 let title = document.createElement('h1');
 title.innerText = 'Minha Lista de Tarefas';
 header.appendChild(title);
+
+// Requisito 2
+let paragrafo = document.createElement('p');
+paragrafo.id = 'funcionamento';
+paragrafo.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+header.appendChild(paragrafo);
