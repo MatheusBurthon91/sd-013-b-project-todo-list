5960a07abf41d4e7bb488728dea6d1a65c79d5f3
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-06-02 19:18:33 -0300

    testando

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 window.onload = function(){
 }
 
-
-
 function addItem(){ 
     const listaPai = document.getElementById("lista-tarefas");
     let input = document.getElementById('texto-tarefa').value;
     let novoItem = document.createElement('li');
     novoItem.innerHTML = input;
     listaPai.appendChild(novoItem);
-    input.reset();
+    document.querySelector('input').value = '';
 }
 
 let button = document.querySelector("button");
 button.addEventListener("dblclick", addItem);
-
