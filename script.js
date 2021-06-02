8f4679528632914aba5706dbcaa94c05db7d7558
Alef Sloan <Alef_sloan@hotmail.com>
2021-06-02 16:57:32 -0300

    Requisito 7 e 8 finalizado, respeitando o lint

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const buttonText = document.getElementById('criar-tarefa');
+const listItem = document.getElementsByTagName('li');
+console.log(listItem);
 
+function selectedItem(event) {
+  const item = event.target;
+  const selects = document.getElementsByClassName('selected');
+  if (item.classList.contains('selected')) {
+    // empty
+  } else if (selects.length === 0) {
+    item.classList.add('selected');
+  } else {
+    selects[0].classList.remove('selected');
+    item.classList.add('selected');
+  }
+}
 function clickAddList() {
   const mainList = document.getElementById('lista-tarefas');
   const inputText = document.getElementById('texto-tarefa');
   const lineList = document.createElement('li');
-  console.log(inputText.value)
+  lineList.className = 'lista';
   lineList.innerText = inputText.value;
-  mainList.appendChild(lineList)
+  mainList.appendChild(lineList);
   inputText.value = '';
+  mainList.lastElementChild.addEventListener('click', selectedItem);
 }
 
 buttonText.addEventListener('click', clickAddList);
