175aee2c8c05bc4644dc233298cb44c6ec032fae
Rafael Friederick <rafael.mf.documents@gmail.com>
2021-06-02 15:39:25 -0300

    Desafios 5, 6 e 7

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.onload = () => {
+function inputTaskinList() {
+  const inputTaskValue = document.getElementById('texto-tarefa').value;
+  if (inputTaskValue) {
+    const todoList = document.getElementById('lista-tarefas');
+    const listItem = document.createElement('li');
+    listItem.innerText = inputTaskValue;
+    todoList.appendChild(listItem);
+  }
+  document.getElementById('texto-tarefa').value = '';
+}
 
+window.onload = () => {
+  const createTaskButton = document.getElementById('criar-tarefa');
+  createTaskButton.addEventListener('click', inputTaskinList);
 };
