df15e79d5cfba2eaf7ff49b552b0e6ee08992717
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-06-02 18:41:51 -0300

    RQ 7 - Alterna seleçao do item da lista

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function adicionarItem() {
   let itemList = document.createElement("li");
   let texto = document.createTextNode(document.getElementById("texto-tarefa").value);
   itemList.appendChild(texto);
+  criaObjetoClicável(itemList);
   document.getElementById('lista-tarefas').appendChild(itemList);
   document.getElementById("texto-tarefa").value = '';
+}
+
+function criaObjetoClicável(objeto) {
+  objeto.addEventListener('click', selecao);
+}
+
+function selecao(objeto) {
+  elemento = objeto.target;
+  elemento.classList.toggle('selecionado');
 }
