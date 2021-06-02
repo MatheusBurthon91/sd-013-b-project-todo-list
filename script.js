9ee2a2abc773c32341569cc62a67baf81fc38b76
André Sena <andresena9653@gmail.com>
2021-06-02 20:22:24 -0300

    resolução exercicio 12

--- script.js
+++ script.js
@@ -45,12 +45,26 @@ function addEventClear() { // essa função adiciona evento de apagar todas as t
 function clearFinished() { // essa função ao ser chamada limpa todas as tarefas concluídas da lista;
   const lista = document.querySelectorAll('.completed');
   for (let index = 0; index < lista.length; index += 1) {
-    lista[index].parentNode.removeChild(lista[index])
+    lista[index].parentNode.removeChild(lista[index]);
   }
 }
 function addEventClearFinished() { // essa função adiciona evento de apagar todas as tarefas concluídas, chamando a função anterior;
   const elemento = document.querySelector('#remover-finalizados');
   elemento.addEventListener('click', clearFinished);
+}
+function listSaved() {
+  let object = {
+    tasks: document.querySelector('#lista-tarefas').innerHTML,
+  };
+  localStorage.setItem('tasks', object.tasks);
+}
+function addEventSaved() {
+  const elemento = document.querySelector('#salvar-tarefas');
+  elemento.addEventListener('click', listSaved);
+}
+function addElementsSaved() {
+  const tasks = localStorage.getItem('tasks');
+  document.querySelector('#lista-tarefas').innerHTML = tasks;
 }
 function taskSubmit() { // função que adiciona o elemento com o valor do input, e chama a função anterior para adicionar evento que muda o fundo;
   const taskValue = document.querySelector('#texto-tarefa').value;
@@ -64,6 +78,8 @@ function taskSubmit() { // função que adiciona o elemento com o valor do input
   addEventListSublinha();
   addEventClear();
   addEventClearFinished();
+  addEventSaved();
 }
 const taskBtn = document.querySelector('#criar-tarefa');
 taskBtn.addEventListener('click', taskSubmit); // evento que adiciona tarefa a lista;
+addElementsSaved();
