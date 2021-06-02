8f0aff082b77b92d2bc16e7d9d8f25f20b61d46f
Rodrigo Marchi Silva <rodrigomarchisilva@gmail.com>
2021-06-02 15:42:07 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ let list = document.getElementById('lista-tarefas');
 
 // Adicionar uma tarefa na ol
 function addLi(listPath) {
-  input = document.getElementById('texto-tarefa');
-  value = input.value;
-  newLi = document.createElement('li');
+  let input = document.getElementById('texto-tarefa');
+  let value = input.value;
+  let newLi = document.createElement('li');
   newLi.innerText = value;
   listPath.appendChild(newLi);
   input.value = '';
 }
 
+// Pintar o background de cinza
+function backgroundPainter(element) {
+  element.className = 'grey';
+}
+
+// Tornar o fundo de um item da lista cinza
+function greyBackground(listPath) {
+  let liArray = listPath.children;
+  for (let index = 0; index < liArray.length; index += 1) {
+    liArray[index].onclick = function() {backgroundPainter(liArray[index])}
+  }
+}
+
 // Clicar no botão para adicionar uma tarefa
-creationButton.onclick = function() {addLi(list)}
+creationButton.onclick = function() {
+  addLi(list);
+  greyBackground(list);
+}
