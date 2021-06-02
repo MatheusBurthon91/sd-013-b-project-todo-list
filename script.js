11826a083605ae4fb7bf294bb88da677baa9a92a
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-06-02 16:02:39 -0300

    7

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let tarefa = document.querySelector('#criar-tarefa');
 function criarTarefa(){
   let input = document.querySelector('#texto-tarefa').value;
   let createLi = document.createElement('li');
-  ol.appendChild(createLi).innerText = input
+  ol.appendChild(createLi).innerText = input;
   if (input === input) {
     document.querySelector('#texto-tarefa').value = null;
   }
@@ -13,4 +13,11 @@ function criarTarefa(){
 
 tarefa.addEventListener('click', criarTarefa)
 
+function colorItem() {
+  let listadeTaks = document.querySelectorAll("#lista-tarefas li");
+  for (let i = 0; i < listadeTaks.length; i += 1) {
+    listadeTaks[i].style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
 
+ol.addEventListener('click', colorItem)
