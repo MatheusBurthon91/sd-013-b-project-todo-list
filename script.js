c120fa8a59cc61f0a09a247ea6fda6a76139734c
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-02 19:02:47 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -12,7 +12,17 @@ buttonCreateTask.addEventListener('click', () => {
 const taskList = document.querySelector('#lista-tarefas');
 
 taskList.addEventListener('click', (event) => {
-  const assistant = event.target;
-  console.log(assistant);
-  assistant.style.backgroundColor = 'rgb(128, 128, 128)';
+  const assistant = event;
+  const classToRemove = document.querySelector('.selected');
+  console.log(classToRemove);
+  if (classToRemove === null) {
+    assistant.target.classList.add('selected');
+    assistant.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    console.log(classToRemove);
+  } else {
+    classToRemove.classList.remove('selected');
+    classToRemove.style.backgroundColor = 'white';
+    assistant.target.classList.add('selected');
+    assistant.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
 });
