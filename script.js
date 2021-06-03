7c9e48dcf3d10fcf70802c91025de27274f896f7
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-06-02 22:48:16 -0300

    Realizado requisito 7

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ function addChore(){
   let choreList = document.getElementById('lista-tarefas');
   newListItem.innerText = chore;
   choreList.appendChild(newListItem);
+  newListItem.classList.add('chores');
   document.getElementById('texto-tarefa').value = '';
 }
 
+// Evento de clicar no botão para adicionar a tarefa
 let clickButton = document.getElementById('criar-tarefa');
 clickButton.addEventListener('click', addChore);
+
+// Função para alterar o fundo dos itens da lista para rgb(128,128,128);
+function listItemColor(event) {
+  const selectItem = event;
+  if (selectItem.target.classList.contains('chores')){
+    selectItem.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
+}
+
+const selectList = document.getElementById('lista-tarefas');
+selectList.addEventListener('click', listItemColor);
