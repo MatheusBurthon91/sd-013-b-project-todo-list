e48b3ab8a37265ad5b74140ca5246d15099feea1
k4nl <gustavoh.bh@gmail.com>
2021-06-03 02:02:26 -0300

    resoluçao desafio 7

--- script.js
+++ script.js
@@ -13,6 +13,27 @@ function addEventListenerButton() {
   addElementButton.addEventListener('click', createLi);
 }
 
+function newClass(event) {
+  const tasks = document.querySelectorAll('.tarefa'); // sempre que for um array percorre ele com o for
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+function addNewClassEventListener() {
+  const ol = document.querySelector('#lista-tarefas');
+  ol.addEventListener('click', newClass);
+}
+
+/*
+function addListaDeTarefasEventListener() {
+  const ol = document.querySelector('#lista-tarefas');
+  ol.addEventListener('click', setNewColor);
+}
+*/
+
 window.onload = () => {
   addEventListenerButton();
+  addNewClassEventListener();
 };
