1b7ebaf5d0c23c21a081d8795d0d8b18dbb375b3
diogodlemos <diogodrummond@hotmail.com>
2021-06-02 23:18:25 -0300

    Requisitos 9 e 10 que apaga toda a lista e apaga tarefas concluidas

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function createButton() {
 }
 
 createButton();
-
+//função para selecionar item clicado e mudar bg
 function setBackGroundColor(event) {
   const itemList = document.querySelectorAll('.item');
   for(index = 0; index < itemList.length; index += 1) {
@@ -51,7 +51,7 @@ function setBackGroundColor(event) {
     event.target.classList.add('selected');
   }
 }   
-
+//função para riscar tarefas cumpridas
 function actionCompleted(event) {
     if (event.target.classList.contains('completed')) {
         event.target.classList.remove('completed');
@@ -59,7 +59,7 @@ function actionCompleted(event) {
         event.target.classList.add('completed');
     }
 }
-
+//função para adcionar tarefas
 function addAssignment() {
   const list = document.querySelector('#lista-tarefas');
   const input = document.querySelector('#texto-tarefa');
@@ -74,3 +74,36 @@ function addAssignment() {
 
 const buttonAdd = document.querySelector('#criar-tarefa');
 buttonAdd.addEventListener('click',addAssignment);
+//função para apagar toda a lista
+function clearList() {
+  const elementFather = document.getElementsByTagName('ol')[0];
+  const itemList = document.querySelectorAll('.item');
+  for(let index = 0; index < itemList.length; index += 1) {
+    elementFather.removeChild(itemList[index]);
+  } 
+}
+//Criando Botão Apagar Lista
+const elementFather = document.body;
+const buttonClear = document.createElement('button');
+buttonClear.id = 'apaga-tudo';
+buttonClear.innerText = 'Apagar Lista';
+buttonClear.style.marginLeft = '10px';
+buttonClear.addEventListener('click', clearList);
+elementFather.appendChild(buttonClear);
+//função que apaga os itens da lista marcados como finalizados
+function clearFinished() {
+  const elementFather = document.querySelector('#lista-tarefas');  
+  const listFinished = document.querySelectorAll('.completed');
+  for(let index = 0; index < listFinished.length; index += 1) {
+      elementFather.removeChild(listFinished[index]);
+  }  
+}
+
+//Criando Botão Apaga tarefas finalizadas
+const buttonClearFinished = document.createElement('button');
+buttonClearFinished.id ='remover-finalizados';
+buttonClearFinished.innerText = 'Apaga Tarefas Finalizadas';
+buttonClearFinished.style.marginLeft = '10px';
+buttonClearFinished.addEventListener('click',clearFinished);
+elementFather.appendChild(buttonClearFinished);
+
