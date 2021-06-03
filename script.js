a2eb8ce4622eab18e331e19048cf760ecb43f4dd
Rodrigo Marchi Silva <rodrigomarchisilva@gmail.com>
2021-06-03 20:58:18 -0300

    Correção do eslint (dale refatoração...)

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // Variáveis iniciais de captura de elementos
-let creationButton = document.getElementById('criar-tarefa');
-let list = document.getElementById('lista-tarefas');
-let removeAllButton = document.getElementById('apaga-tudo');
-let removeCompletedButton = document.getElementById('remover-finalizados');
-let saveChangesButton = document.getElementById('salvar-tarefas');
-let mensagem = document.getElementById("mensagem-alerta");
-let upwardButton = document.getElementById('mover-cima');
-let downwardButton = document.getElementById('mover-baixo');
-let removeSelectedButton = document.getElementById('remover-selecionado');
+const creationButton = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const removeAllButton = document.getElementById('apaga-tudo');
+const removeCompletedButton = document.getElementById('remover-finalizados');
+const saveChangesButton = document.getElementById('salvar-tarefas');
+const mensagem = document.getElementById('mensagem-alerta');
+const upwardButton = document.getElementById('mover-cima');
+const downwardButton = document.getElementById('mover-baixo');
+const removeSelectedButton = document.getElementById('remover-selecionado');
 
 // Recuperar alterações através do localStorage
-if (typeof(Storage) !== "undefined") {
-  let changes = localStorage.getItem("listChanges");
+if (typeof (Storage) !== 'undefined') {
+  const changes = localStorage.getItem('listChanges');
   list.innerHTML = changes;
 } else {
-  mensagem.innerHTML = "Rapaz, eta navegadorzinho ruim esse teu, hein?";
+  mensagem.innerHTML = 'Rapaz, eta navegadorzinho ruim esse teu, hein?';
 }
 
 // Adicionar uma tarefa na ol
 function addLi(listPath) {
-  let input = document.getElementById('texto-tarefa');
-  let value = input.value;
-  let newLi = document.createElement('li');
-  newLi.innerText = value;
+  const input = document.getElementById('texto-tarefa');
+  const wantedValue = input.value;
+  const newLi = document.createElement('li');
+  newLi.innerText = wantedValue;
   listPath.appendChild(newLi);
   input.value = '';
 }
 
 // Auxiliar a pintar o background da li selecionada de cinza
 function greyBackgroundHelper(element) {
-  let searchingForGrey = document.querySelector('.grey');
+  const searchingForGrey = document.querySelector('.grey');
   if (searchingForGrey === null) {
     element.classList.add('grey');
   } else {
@@ -40,9 +40,9 @@ function greyBackgroundHelper(element) {
 
 // Pintar o background da li selecionada de cinza
 function greyBackground(listPath) {
-  let liArray = listPath.children;
+  const liArray = listPath.children;
   for (let index = 0; index < liArray.length; index += 1) {
-    liArray[index].onclick = function() {greyBackgroundHelper(liArray[index])}
+    liArray[index].onclick = function () { greyBackgroundHelper(liArray[index]); };
   }
 }
 
@@ -57,136 +57,138 @@ function markHelper(element) {
 
 // Riscar item da lista
 function mark(listPath) {
-  let liArray = listPath.children;
+  const liArray = listPath.children;
   for (let index = 0; index < liArray.length; index += 1) {
-    liArray[index].ondblclick = function() {markHelper(liArray[index])}
+    liArray[index].ondblclick = function () { markHelper(liArray[index]); };
   }
 }
 
 // Esvaziar conteúdo da ol
 function removeAll(listPath) {
-  listPath.innerHTML = '';
+  const siteList = listPath;
+  siteList.innerHTML = '';
 }
 
 // Apagar os itens finalizados
 function removeCompleted() {
-  let completedArray = document.getElementsByClassName('completed');
+  const completedArray = document.getElementsByClassName('completed');
   for (let index = completedArray.length - 1; index >= 0; index -= 1) {
     completedArray[index].parentNode.removeChild(completedArray[index]);
   }
 }
 
 // Salvar lista no localStorage
-function saveListChanges () {
-  if (typeof(Storage) !== "undefined") {
-    localStorage.setItem("listChanges", document.getElementById('lista-tarefas').innerHTML);
+function saveListChanges() {
+  if (typeof (Storage) !== 'undefined') {
+    localStorage.setItem('listChanges', document.getElementById('lista-tarefas').innerHTML);
   } else {
-    document.getElementById("mensagem-alerta").innerHTML = "Rapaz, eta navegadorzinho ruim esse teu, hein?";
+    const alerting = document.getElementById('mensagem-alerta');
+    alerting.innerHTML = 'Rapaz, eta navegadorzinho ruim esse teu, hein?';
   }
+}
+
+// Auxiliar da função que move para cima
+function moveUpwardHelper(i, na) {
+  const dataArray = na;
+  const value1 = dataArray[i].innerText;
+  const value2 = dataArray[i - 1].innerText;
+  const classes1 = dataArray[i].className;
+  if (dataArray[i - 1].className !== '') {
+    const classes2 = dataArray[i - 1].className;
+    dataArray[i].className = classes2;
+  }
+  dataArray[i].innerText = value2;
+  dataArray[i].className = '';
+  dataArray[i - 1].innerText = value1;
+  dataArray[i - 1].className = '';
+  dataArray[i - 1].className = classes1;
 }
 
 // Mover o elemento selecionado para cima
 function moveUpward(listPath) {
-  let greyLi = document.querySelector('.grey');
-  let neededArray = listPath.childNodes;
+  const greyLi = document.querySelector('.grey');
+  const neededArray = listPath.childNodes;
   for (let index = 1; index < neededArray.length; index += 1) {
     if (neededArray[index] === greyLi) {
-      let classes1 = neededArray[index].className;
-      let value1 = neededArray[index].innerText;
-      let value2 = neededArray[index - 1].innerText;
-      if (neededArray[index - 1].className !== '') {
-        let classes2 = neededArray[index - 1].className;
-        neededArray[index].innerText = value2;
-        neededArray[index].className = '';
-        neededArray[index].className = classes2;
-        neededArray[index - 1].innerText = value1;
-        neededArray[index - 1].className = '';
-        neededArray[index - 1].className = classes1;
-      } else {
-        neededArray[index].innerText = value2;
-        neededArray[index].className = '';
-        neededArray[index - 1].innerText = value1;
-        neededArray[index - 1].className = '';
-        neededArray[index - 1].className = classes1;
-      }
+      moveUpwardHelper(index, neededArray);
     }
   }
 }
 
+// Auxiliar da função que move para baixo
+function moveDownwardHelper(i, na) {
+  const dataArray = na;
+  const value1 = dataArray[i].innerText;
+  const value2 = dataArray[i + 1].innerText;
+  const classes1 = dataArray[i].className;
+  if (dataArray[i + 1].className !== '') {
+    const classes2 = dataArray[i + 1].className;
+    dataArray[i].className = classes2;
+  }
+  dataArray[i].innerText = value2;
+  dataArray[i].className = '';
+  dataArray[i + 1].innerText = value1;
+  dataArray[i + 1].className = '';
+  dataArray[i + 1].className = classes1;
+}
+
 // Mover o elemento selecionado para baixo
 function moveDownward(listPath) {
-  let greyLi = document.querySelector('.grey');
-  let neededArray = listPath.childNodes;
+  const greyLi = document.querySelector('.grey');
+  const neededArray = listPath.childNodes;
   for (let index = neededArray.length - 2; index >= 0; index -= 1) {
     if (neededArray[index] === greyLi) {
-      let classes1 = neededArray[index].className;
-      let value1 = neededArray[index].innerText;
-      let value2 = neededArray[index + 1].innerText;
-      if (neededArray[index + 1].className !== '') {
-        let classes2 = neededArray[index + 1].className;
-        neededArray[index].innerText = value2;
-        neededArray[index].className = '';
-        neededArray[index].className = classes2;
-        neededArray[index + 1].innerText = value1;
-        neededArray[index + 1].className = '';
-        neededArray[index + 1].className = classes1;
-      } else {
-        neededArray[index].innerText = value2;
-        neededArray[index].className = '';
-        neededArray[index + 1].innerText = value1;
-        neededArray[index + 1].className = '';
-        neededArray[index + 1].className = classes1;
-      }
+      moveDownwardHelper(index, neededArray);
     }
   }
 }
 
 // Remover o elemento selecionado
 function removeSelected() {
-  let toBeDeleted = document.querySelector('.grey');
+  const toBeDeleted = document.querySelector('.grey');
   toBeDeleted.parentNode.removeChild(toBeDeleted);
 }
 
 // Clicar no botão para adicionar uma tarefa
-creationButton.onclick = function() {
+creationButton.onclick = function () {
   addLi(list);
   greyBackground(list);
   mark(list);
 };
 
 // Clicar no botão para apagar a lista toda
-removeAllButton.onclick = function() {
+removeAllButton.onclick = function () {
   removeAll(list);
 };
 
 // Clicar no botão apaga os itens finalizados
-removeCompletedButton.onclick = function() {
-  removeCompleted(list);
+removeCompletedButton.onclick = function () {
+  removeCompleted();
   greyBackground(list);
   mark(list);
 };
 
 // Clicar no botão vai salvar a lista no localStorage
-saveChangesButton.onclick = function() {
+saveChangesButton.onclick = function () {
   saveListChanges();
 };
 
 // Clicar no botão irá mover o elemento da lista selecionado para a posição de cima
-upwardButton.onclick = function() {
+upwardButton.onclick = function () {
   moveUpward(list);
   greyBackground(list);
   mark(list);
 };
 
 // Clicar no botão irá mover o elemento da lista selecionado para a posição de baixo
-downwardButton.onclick = function() {
+downwardButton.onclick = function () {
   moveDownward(list);
   greyBackground(list);
   mark(list);
 };
 
 // Clicar no botão irá remover o elemento selecionado
-removeSelectedButton.onclick = function() {
+removeSelectedButton.onclick = function () {
   removeSelected();
   greyBackground(list);
   mark(list);
