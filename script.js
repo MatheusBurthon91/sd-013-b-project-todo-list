322f5d8c9efcfc51bf25564884b857b1c0afa13b
luis carlos <luizcruz0809@gmail.com>
2021-06-03 17:32:54 -0300

    add requisito 01 ao 06

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// let botaoCria =  document.querySelector('#criar-tarefa');
-// botaoCria.addEventListener('click', addItem);
 
-// let lista = document.querySelector('#lista-tarefas');
 
-// function addItem(){
-//   let item = document.createElement('li');
-//   lista.appendChild(item);
-//   item.innerHTML = 'luis';
-// }
+let botaoCria = document.querySelector('#criar-tarefa');
+botaoCria.addEventListener('click', input);
+
+let lista = document.querySelector('#lista-tarefas');
+
+function input (){
+  
+  let item = document.createElement('li');
+  lista.appendChild(item);
+
+  let input = document.querySelector('#texto-tarefa');
+  
+  let valor = input.value;
+
+  item.innerHTML = valor;
+
+  input.value = '';
+}
