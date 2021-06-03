f3a8edb846a4203685c818851f245799c47b8e09
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-06-03 13:39:58 -0300

    Realiza requsitos 5 e 6

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ inputTasks.id = 'texto-tarefa';
 inputTasks.type = 'text';
 inputTasks.placeholder = 'Digite aqui sua tarefa...';
 inputTasks.size = '50';
+inputTasks.className = 'item-container';
 containerTasks.appendChild(inputTasks);
+
+const btnCreateTasks = document.createElement('button');
+btnCreateTasks.id = 'criar-tarefa';
+btnCreateTasks.innerText = 'Adiconar';
+btnCreateTasks.className = 'item-container';
+containerTasks.appendChild(btnCreateTasks);
 
 const ordemList = document.createElement('ol');
 ordemList.id = 'lista-tarefas';
 containerTasks.appendChild(ordemList);
+
+btnCreateTasks.addEventListener('click', () => {
+  const itemList = document.createElement('li');
+  itemList.innerText = inputTasks.value;
+  itemList.className = 'item-list';
+  ordemList.appendChild(itemList);
+  inputTasks.value = '';
+  localStorage.setItem('tasks', itemList.innerText);
+});
