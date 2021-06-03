729f75d94c8967e83eb799ebed09aba96a645747
luis carlos <luizcruz0809@gmail.com>
2021-06-03 15:58:32 -0300

    add 1 ao 4

---
+++ script.js
@@ -0,0 +1,10 @@
+// let botaoCria =  document.querySelector('#criar-tarefa');
+// botaoCria.addEventListener('click', addItem);
+
+// let lista = document.querySelector('#lista-tarefas');
+
+// function addItem(){
+//   let item = document.createElement('li');
+//   lista.appendChild(item);
+//   item.innerHTML = 'luis';
+// }
