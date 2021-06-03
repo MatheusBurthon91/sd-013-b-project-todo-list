1e831ced9f9ae52d08f571a598a0dffd0c95abdd
Othyose <arth_nf@hotmail.com>
2021-06-03 17:52:14 -0300

    Requisito 7 e correções

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let itemLista;
 let tarefa;
 let textoTarefa;
-let lista = document.getElementById('lista-tarefas');
+const lista = document.getElementById('lista-tarefas');
 const botao = document.getElementById('criar-tarefa');
+let listaSelect = document.querySelector('#lista-tarefas');
 
-function addToList(){
+function addToList() {
   tarefa = document.getElementById('texto-tarefa').value;
   textoTarefa = document.createTextNode(tarefa);
-  itemLista = document.createElement('li')
+  itemLista = document.createElement('li');
+  itemLista.classList.add('list');
   lista.appendChild(itemLista);
   itemLista.appendChild(textoTarefa);
   document.getElementById('texto-tarefa').value = ('');
 }
 
 botao.addEventListener('click', addToList);
+
+function pintaFundo(event) {
+  let clicado = event.target;
+  if (clicado.id != 'lista-tarefas'){
+    clicado.classList.toggle('listBackground');
+  }
+}
+listaSelect.addEventListener('click', pintaFundo);
