2fc479ff389bb31f970284dc608586ce003cacc4
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-06-02 21:23:54 -0300

    RQ 8 - Switch o elemento selecionado.

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function adicionarItem() {
-  let itemList = document.createElement("li");
-  let texto = document.createTextNode(document.getElementById("texto-tarefa").value);
-  itemList.appendChild(texto);
-  criaObjetoClicável(itemList);
-  document.getElementById('lista-tarefas').appendChild(itemList);
-  document.getElementById("texto-tarefa").value = '';
+function selecao(objeto) {
+  let elemento = objeto.target;
+  let lista = document.getElementById('lista-tarefas').children;
+  for (let index = 0; index < lista.length; index += 1) {
+    //if (lista[index].classList.contains('selecionado') && elemento !== lista[index]) {
+      lista[index].classList.remove('selecionado');
+    //}
+  }
+  elemento.classList.toggle('selecionado');
 }
 
-function criaObjetoClicável(objeto) {
+function criaObjetoClicavel(objeto) {
   objeto.addEventListener('click', selecao);
 }
 
-function selecao(objeto) {
-  elemento = objeto.target;
-  elemento.classList.toggle('selecionado');
+function adicionarItem() {
+  let itemList = document.createElement('li');
+  let texto = document.createTextNode(document.getElementById('texto-tarefa').value);
+  itemList.appendChild(texto);
+  criaObjetoClicavel(itemList);
+  document.getElementById('lista-tarefas').appendChild(itemList);
+  document.getElementById('texto-tarefa').value = '';
 }
