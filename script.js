0eaf69c4ad9e3b5c0423533107a7ff566fc3e75f
rodrigocdfidencio <rodrigo.fidencio@gmail.com>
2021-06-03 10:31:39 -0300

    Requisito 5 e 6 funcionando

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const inputText = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 const olTaskList = document.getElementById('lista-tarefas');
 
+
 function createTask() {
-  const task = inputText.nodeValue;
+  const task = inputText.value;
   const li = document.createElement('li');
   li.innerText = task;
   olTaskList.appendChild(li);
+  inputText.value = '';
 }
 
 btnCreateTask.addEventListener('click', createTask);
