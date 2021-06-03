4f98ba054a6b13fac6e4acd60d508b828cf2a36c
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-06-03 10:45:50 -0300

    Complete fase 7.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const taskList = document.getElementById('lista-tarefas');
 
+const changeBGColor = (event) => {
+  const li = event.target;
+  li.style.backgroundColor = 'rgb(128, 128, 128)';
+};
+
+const createTask = (taskName) => {
+  const li = document.createElement('li');
+  li.addEventListener('click', changeBGColor);
+  li.innerText = taskName;
+  return li;
+};
+
 const saveTaskList = () => {
   const list = Array.from(taskList.children).map((li) => li.textContent);
   localStorage.removeItem('task-list');
@@ -11,8 +23,7 @@ const setTaskList = () => {
 
   if (!list) return;
   list.split(',').forEach((task) => {
-    const li = document.createElement('li');
-    li.innerText = task;
+    const li = createTask(task);
     taskList.appendChild(li);
   });
 };
@@ -21,9 +32,8 @@ const addTaskOnList = (event) => {
   event.preventDefault();
 
   const data = Array.from(event.target.children);
-  const li = document.createElement('li');
+  const li = createTask(data[0].value);
 
-  li.innerText = data[0].value;
   data[0].value = '';
   taskList.appendChild(li);
   saveTaskList();
