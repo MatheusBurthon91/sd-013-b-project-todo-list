b3d17f72026b152d3fecbded53f14837b682950c
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-06-03 17:45:31 -0300

    modificao no script , atendendo o requisito  6

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let buttonAdicionar = document.querySelector('#criar-tarefa');
 let listaOrdenada = document.querySelector('#lista-tarefas');
+let li = document.querySelector('.lista');
 function tarefa() {
   let textInp = document.querySelector('#texto-tarefa');
-  let listaOrdenada = document.querySelector('#lista-tarefas');
   let creatLi = document.createElement('li');
+  creatLi.className = 'lista';
   listaOrdenada.appendChild(creatLi);
   creatLi.innerText = textInp.value;
-  console.log(creatLi);
   textInp.value = '';
 }
 buttonAdicionar.addEventListener('click', tarefa);
+
+// criar uma funcao que ao clicar no botao o elemento da lista fique com a cor cinza
+
+function colorElement() {
+  let list = document.querySelector('.lista');
+  list.style.background = 'rgb(128, 128, 128)';
+}
+
+listaOrdenada.addEventListener('click', colorElement);
