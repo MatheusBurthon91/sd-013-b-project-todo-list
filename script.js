039b0599c41ab25f7fe21cd21e12ba2be83365e7
Btriz <00btrz@gmail.com>
2021-06-03 18:32:50 -0300

    desafios 7 ao 9

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-
-  function adicionarTarefa() {
-    const lista = document.getElementById('lista-tarefas');
-    const item = document.createElement('li');
-    const entrada = document.getElementById('texto-tarefa');
-    item.innerText = entrada.value;
-    lista.appendChild(item);
-    entrada.value = '';
+function selecionarItem(itemEvento) {
+  const selecionado = document.querySelector('.selecionado');
+  const clicado = itemEvento.target;
+  if (selecionado !== null) {
+    selecionado.className = '';
+    clicado.className = 'selecionado';
+  } else {
+    clicado.className = 'selecionado';
   }
+}
 
-  const botaoCriar = document.getElementById('criar-tarefa');
-  botaoCriar.addEventListener('click', adicionarTarefa);
+function adicionarTarefa() {
+  const lista = document.getElementById('lista-tarefas');
+  const novoItem = document.createElement('li');
+  const entrada = document.getElementById('texto-tarefa');
+  novoItem.innerText = entrada.value;
+  novoItem.addEventListener('click', selecionarItem);
+  lista.appendChild(novoItem);
+  entrada.value = '';
+}
 
+const botaoCriar = document.getElementById('criar-tarefa');
+
+botaoCriar.addEventListener('click', adicionarTarefa);
