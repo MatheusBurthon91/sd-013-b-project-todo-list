5f917e176efc44766285c367874139b7f92694f8
rodrigocdfidencio <rodrigo.fidencio@gmail.com>
2021-06-03 13:34:14 -0300

    Correções no lint e ESLint

--- script.js
+++ script.js
@@ -2,7 +2,43 @@ const inputText = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 const olTaskList = document.getElementById('lista-tarefas');
 
+function clearSelected() {
+  const taskCreated = document.querySelectorAll('.task');
+  for (let index = 0; index < taskCreated.length; index += 1) {
+    const itemSelected = taskCreated[index].className;
+    taskCreated[index].className = itemSelected.replace(/-item-selected/, '');
+  }
+}
 
+function taskSelected(event) {
+  const taskCreated = document.querySelectorAll('.task');
+  clearSelected();
+  // console.log("entrou")
+  const selected = event.target;
+  for (let index = 0; index < taskCreated.length; index += 1) {
+    if (taskCreated[index] === selected) {
+      selected.classList.toggle('task-item-selected');
+    }
+  }
+}
+
+function completedTask(event) {
+  const taskClick = event.target;
+  const classTaskClick = taskClick.className;
+  if (classTaskClick.includes(' completed')) {
+    taskClick.className = classTaskClick.replace(/completed/, '');
+  } else {
+    taskClick.className += ' completed';
+  }
+}
+
+function createClickable() {
+  const taskCreated = document.querySelectorAll('.task');
+  for (let index = 0; index < taskCreated.length; index += 1) {
+    taskCreated[index].addEventListener('click', taskSelected);
+    taskCreated[index].addEventListener('dblclick', completedTask);
+  }
+}
 
 function createTask() {
   const task = inputText.value;
@@ -16,42 +52,3 @@ function createTask() {
 }
 
 btnCreateTask.addEventListener('click', createTask);
-
-function taskSelected(event) {
- let taskCreated = document.querySelectorAll('.task');
- clearSelected();
-    //console.log("entrou")
-    const selected = event.target;
-   for (let index = 0; index < taskCreated.length; index += 1) {
-       if (taskCreated[index] === selected) {
-        selected.classList.toggle('task-item-selected')
-       } 
-      }
-      }
-
-function clearSelected(){
-    let taskCreated = document.querySelectorAll('.task');
-    for (index = 0; index < taskCreated.length; index += 1){
-    const itemSelected = taskCreated[index].className
-    taskCreated[index].className = itemSelected.replace(/-item-selected/, '');
-    }
-}
-
-function createClickable() {
-    let taskCreated = document.querySelectorAll('.task');
-    for (let index = 0; index < taskCreated.length; index += 1) {
-        taskCreated[index].addEventListener('click', taskSelected)
-        taskCreated[index].addEventListener('dblclick', completedTask)
-    }
-}
-
-function completedTask (event){
-    const taskClick = event.target
-    const classTaskClick = taskClick.className;
-  if (classTaskClick.includes(' completed')) {
-    taskClick.className = classTaskClick.replace(/completed/, '');
-  } else {
-    taskClick.className += ' completed';
-  }
-}
-
