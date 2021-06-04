268f94bc7939f94a06a3d74ca56431183d57f4e7
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-04 11:19:06 -0300

    cumpre os requisitos de 1 a 4 do projeto todo list

--- script.js
+++ script.js
@@ -13,5 +13,37 @@ paragraphHeader.id = 'funcionamento';
 const inputSection = document.createElement('section');
 document.body.appendChild(inputSection);
 const input = document.createElement('input');
+input.setAttribute ('type', 'text');
+input.setAttribute ('value', '');
 inputSection.appendChild(input);
 input.id = 'texto-tarefa';
+const todoListDiv = document.createElement('div');
+document.body.appendChild(todoListDiv);
+const todoList = document.createElement('ol');
+todoListDiv.appendChild(todoList);
+todoList.id = 'lista-tarefas';
+const divButtons = document.createElement('div');
+document.body.appendChild(divButtons);
+const listAddButton = document.createElement('button');
+listAddButton.id = 'criar-tarefa';
+listAddButton.innerText = 'Adicionar tarefa';
+divButtons.appendChild(listAddButton);
+function addItem() {
+  const orderList = document.querySelector('#lista-tarefas');
+  console.log(orderList);
+  const taskButton = document.querySelector('#criar-tarefa');
+  // desafio cumprido com auxílio de estudos do w3Scholl, 
+  // uso de input e valores.
+  taskButton.addEventListener('click', (event) => {
+    let inputId = document.getElementById('texto-tarefa').value;
+    let listItem = document.createElement('li');
+    event.preventDefault();
+    listItem.innerText = inputId;
+    orderList.appendChild(listItem);
+    if (listItem.innerText === inputId){
+      inputId[0].value.replace = '';
+    } 
+  });
+
+}
+addItem();
