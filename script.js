1a6b277db5c00a979cbe48a6f7bdee2666a4f1b2
Rafael Romano <rafa-kun@hotmail.com>
2021-06-03 23:43:17 -0300

    adicionando exercicio 12 e arrumando lint

--- script.js
+++ script.js
@@ -2,58 +2,90 @@ const botaoAdi = document.querySelector('#criar-tarefa');
 const textTarefa = document.querySelector('#texto-tarefa');
 const listaTarefa = document.querySelector('#lista-tarefas');
 const botaoApaga = document.querySelector('#apaga-tudo');
-const botaoFinalizados=document.querySelector('#remover-finalizados');
-const botaoAddTarefas=document.querySelector('#salvar-tarefas');
+const botaoFinalizados = document.querySelector('#remover-finalizados');
+const botaoAddTarefas = document.querySelector('#salvar-tarefas');
 
 function adicionarTarefa() {
-  let input = textTarefa.value;
-  let li = document.createElement('li');
+  const input = textTarefa.value;
+  const li = document.createElement('li');
   li.innerText = input;
-  li.id='li';
+  li.id = 'li';
   textTarefa.value = '';
   listaTarefa.appendChild(li);
 }
 
 botaoAdi.addEventListener('click', adicionarTarefa);
 
+function removendoClasse() {
+  // seleciona o elemento com a classe selected na posição 0;
+  const li = document.getElementsByClassName('selected')[0];
+  // se caso ele achar um li na posição 0 ele vai exclui-lo;
+  if (li) {
+    li.classList.remove('selected');
+  }
+}
+
 listaTarefa.addEventListener('click', (event) => {
   removendoClasse();
 
   event.target.classList.add('selected');
 });
-//Source=Requisito 8 inspirado na resolução do Ygor Lages link:https://github.com/tryber/sd-013-b-project-todo-list/tree/ygor-project-todo-list
 
-function removendoClasse(evento) {
-  //seleciona o elemento com a classe selected na posição 0;
-  let li = document.getElementsByClassName('selected')[0];
-  //se caso ele achar um li na posição 0 ele vai exclui-lo;
-  if (li) {
-    li.classList.remove('selected');
-  }
-}
+// Source=Requisito 8 inspirado na resolução do Ygor Lages link:https://github.com/tryber/sd-013-b-project-todo-list/tree/ygor-project-todo-list
 
 listaTarefa.addEventListener('dblclick', (doubleclick) => {
   doubleclick.target.classList.toggle('completed');
 });
 
 botaoApaga.addEventListener('click', () => {
-  const apagaSelecionado=document.querySelectorAll('li');
-  for(let index=0;index<apagaSelecionado.length;index+=1){
+  const apagaSelecionado = document.querySelectorAll('li');
+  for (let index = 0; index < apagaSelecionado.length; index += 1) {
     apagaSelecionado[index].remove();
   }
 });
+
 botaoFinalizados.addEventListener('click', () => {
-  const apagaFinalizado=document.querySelectorAll('.completed');
-  for(let index=0;index<apagaFinalizado.length;index+=1){
+  const apagaFinalizado = document.querySelectorAll('.completed');
+  for (let index = 0; index < apagaFinalizado.length; index += 1) {
     apagaFinalizado[index].remove();
   }
 });
 
-botaoAddTarefas.addEventListener('click',()=>{
-  let completed=document.querySelectorAll('.completed');
-    
-  localStorage.setItem('li',completed)
+botaoAddTarefas.addEventListener('click', () => {
+  const li = document.querySelectorAll('li');
+  console.log(li);
+  const items = [];
+  const completeds = [];
 
+  for (let index = 0; index < li.length; index += 1) {
+  // vai adicionar o texto do li no array items
+    items.push(li[index].innerText);
+    // se o classlist conter completed e pegar o index
+    if (li[index].classList.contains('completed') === true) {
+      completeds.push(index);
+    }
+  }
+  // adiciona ao local storage como chave items e completeds value
+  localStorage.setItem('items', JSON.stringify(items));
+  localStorage.setItem('completeds', JSON.stringify(completeds));
+});
 
+window.onload = () => {
+  // armazendo os items
+  let items = localStorage.getItem('items');
+  if (items !== null && items.length > 0) {
+  // usa o json para voltar ao array
+    items = JSON.parse(items);
+    const completeds = JSON.parse(localStorage.getItem('completeds'));
 
-})
+    items.forEach((value, index) => {
+      const li = document.createElement('li');
+
+      if (completeds.indexOf(index) !== -1) {
+        li.className = 'completed';
+      }
+      li.innerText = value;
+      listaTarefa.appendChild(li);
+    });
+  }
+};
