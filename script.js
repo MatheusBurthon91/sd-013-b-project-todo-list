553f344ead63261a5ca024160cfbefb43a41080b
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-06-04 04:10:43 -0300

    Requisitos 7, 8 e 9 completos e substituiçao de style.css com nome errado

--- script.js
+++ script.js
@@ -2,13 +2,34 @@ const list = document.getElementById('lista-tarefas');
 let button = document.getElementById('criar-tarefa');
 let input = document.getElementById('texto-tarefa');
 
+
 function createNew() {
-  const listItem = document.createElement('li'); 
-  listItem.innerText = input.value
+  const listItem = document.createElement('li');
+  listItem.innerText = input.value;
   listItem.classList = 'item';
-  list.appendChild(listItem)
+  list.appendChild(listItem);
   input.value = '';
 }
 
+function selecting() {
+  if (event.target.classList.contains('item')) {
+    const lastItem = document.querySelector('.selectedItem');
+    event.target.classList.add('selectedItem');
+    if (lastItem) {
+      lastItem.classList.remove('selectedItem');
+    }
+  }
+}
+
+function completed() {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
 button.addEventListener('click', createNew);
-  
+list.addEventListener('click', selecting);
+list.addEventListener('dblclick', completed);
+list.innerText = localStorage.getItem('list');
