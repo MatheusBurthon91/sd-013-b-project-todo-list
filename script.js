8d640c6d0112fa70f282395076214320aaeb7414
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-06-03 21:16:25 -0300

    Adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const ol = document.getElementById('lista-tarefas');
 
+// Adicionar as tarefas na lista
 function addTask() {
   button.addEventListener('click', function () {
     const liCreate = document.createElement('li');
@@ -12,3 +13,19 @@ function addTask() {
 }
 
 addTask();
+
+// Mudar a cor do background ao clicar em um item da lista
+const li = document.getElementsByTagName('li');
+function changeBackgroundList() {
+  ol.addEventListener('click', function (event) {
+    for (let index = 0; index < li.length; index += 1) {
+      if (li[index].className === 'cinza') {
+        li[index].classList.remove('cinza');
+      }
+      if (li[index].className !== 'cinza') {
+        event.target.classList.add('cinza');
+      }
+    }
+  });
+}
+changeBackgroundList();
