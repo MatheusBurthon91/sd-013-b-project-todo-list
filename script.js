c051cdd4bb5f8b5438217e87379e1ab22351d457
k4nl <gustavoh.bh@gmail.com>
2021-06-04 11:13:12 -0300

    resolucao do desafio 8 e 9

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createLi() {
+function createLi() { // cria o elemento li com a classe tarefa, reseta sempre que a li for criada
   const li = document.createElement('li');
   const ol = document.querySelector('#lista-tarefas');
   const input = document.querySelector('#texto-tarefa');
@@ -7,33 +7,31 @@ function createLi() {
   ol.appendChild(li);
   input.value = '';
 }
-
-function addEventListenerButton() {
+function addEventListenerButton() { // cria o event listener no botao de adicionar
   const addElementButton = document.getElementById('criar-tarefa');
   addElementButton.addEventListener('click', createLi);
 }
-
-function newClass(event) {
+function selectedTask(event) { // cria a classe selected
   const tasks = document.querySelectorAll('.tarefa'); // sempre que for um array percorre ele com o for
   for (let i = 0; i < tasks.length; i += 1) {
     tasks[i].classList.remove('selected');
   }
   event.target.classList.add('selected');
 }
-
-function addNewClassEventListener() {
-  const ol = document.querySelector('#lista-tarefas');
-  ol.addEventListener('click', newClass);
+function completedTask(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
-
-/*
-function addListaDeTarefasEventListener() {
+function addSelectedEventListener() { // cria o event listener nas li's
   const ol = document.querySelector('#lista-tarefas');
-  ol.addEventListener('click', setNewColor);
+  ol.addEventListener('click', selectedTask);
+  ol.addEventListener('dblclick', completedTask);
 }
-*/
 
 window.onload = () => {
   addEventListenerButton();
-  addNewClassEventListener();
+  addSelectedEventListener();
 };
