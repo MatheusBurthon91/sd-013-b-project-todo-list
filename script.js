8e15dcea0a43c46d3379bd485192e3588c3ce3fe
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-06-04 14:29:41 -0300

    verificando evaluator

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ window.onload = function(){
 }
 
 function addItem(){ 
-    const listaPai = document.getElementById("lista-tarefas");
+    const parentElement = document.getElementById("lista-tarefas");
     let input = document.getElementById('texto-tarefa').value;
-    let novoItem = document.createElement('li');
-    novoItem.innerHTML = input;
-    listaPai.appendChild(novoItem);
+    let newItem = document.createElement('li');
+    newItem.innerHTML = input;
+    parentElement.appendChild(newItem);
     document.querySelector('input').value = '';
 }
 
 let button = document.querySelector("button");
 button.addEventListener("click", addItem);
+
+const itemList =  document.querySelector('#lista-tarefas');
+itemList.addEventListener("click", function () {
+    for (let index = 0; index < itemList.children.length; index += 1) {
+     itemList.children[index].classList.toggle('selected');
+    }
+})
+
+
+const itemListCompleted =  document.querySelector('#lista-tarefas');
+itemListCompleted.addEventListener("dblclick", function () {
+    for (let index = 0; index < itemListCompleted.children.length; index += 1) {
+     itemListCompleted.children[index].classList.toggle('completed');
+    }
+})
+
