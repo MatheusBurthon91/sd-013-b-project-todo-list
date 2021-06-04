8d92cc3b432351f0330a3e370422371707625a9a
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-06-04 00:25:41 -0300

    Project To-do list, requisito 1-4 adicionados

---
+++ script.js
@@ -0,0 +1,6 @@
+function createElement () {
+    let list = document.getElementById('lista-de-tarefas');
+    let input = document.getElementById('texto-tarefa');
+    let button = document.getElementById('criar-tarefa');
+    
+}
