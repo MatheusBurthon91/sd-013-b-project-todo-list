b9db7189985c005aab306800468ff8157967d2fd
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-06-04 02:31:27 -0300

    Requisitos 5 e 6 finalizados

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function createElement () {
-    let list = document.getElementById('lista-de-tarefas');
-    let input = document.getElementById('texto-tarefa');
-    let button = document.getElementById('criar-tarefa');
-    
+const list = document.getElementById('lista-tarefas');
+let button = document.getElementById('criar-tarefa');
+let input = document.getElementById('texto-tarefa');
+
+function createNew() {
+  const listItem = document.createElement('li'); 
+  listItem.innerText = input.value
+  listItem.classList = 'item';
+  list.appendChild(listItem)
+  input.value = '';
 }
+
+button.addEventListener('click', createNew);
+  
