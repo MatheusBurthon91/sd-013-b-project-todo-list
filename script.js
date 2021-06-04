9215bfbc9b28126e016bfd4ab44ab9c042d48061
luis carlos <luizcruz0809@gmail.com>
2021-06-04 12:56:52 -0300

    add 01 ao 08

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 let botaoCria = document.querySelector('#criar-tarefa');
 botaoCria.addEventListener('click', input);
 
@@ -9,12 +8,26 @@ function input (){
   
   let item = document.createElement('li');
   lista.appendChild(item);
-
+  item.className = 'pintar';
   let input = document.querySelector('#texto-tarefa');
-  
   let valor = input.value;
-
   item.innerHTML = valor;
-
   input.value = '';
+}
+// -----------------------------------------------------------
+
+lista.addEventListener('click', click);
+
+function click(event) {
+
+  const lastItem = document.querySelector('.cinza');
+
+  if (event.target.classList.contains('pintar')) {
+
+    event.target.classList.add('cinza');
+
+    if (lastItem) {
+    lastItem.classList.remove('cinza');
+    }
+  }
 }
