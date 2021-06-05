c27922fa4fd005bc23e8c05b3418a0db03ac9429
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-06-04 21:21:01 -0300

    modificao no script , atendendo os requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonAdicionar = document.querySelector('#criar-tarefa');
 const listaOrdenada = document.querySelector('#lista-tarefas');
-const lista = document.querySelector('li');
+const buttonPaga= document.querySelector('#apaga-tudo');
 function tarefa() {
   const textInp = document.querySelector('#texto-tarefa');
   const creatLi = document.createElement('li');
@@ -12,9 +12,7 @@ function tarefa() {
 buttonAdicionar.addEventListener('click', tarefa);
 
 function colorElement(event) {
-  console.log(event.target.classList[0]);
-  let classLista = document.querySelector('.lista');
-  console.log(classLista);
+  let classLista = document.querySelector('.lista'); 
   if (classLista !== null) {
     classLista.classList.remove('lista');
   }
@@ -22,3 +20,26 @@ function colorElement(event) {
 }
 
 listaOrdenada.addEventListener('click', colorElement);
+
+function duploClick(event) {
+  let classLista = document.querySelector('.completed');
+  
+  if (classLista !== null) {
+    classLista.classList.remove('completed');     //se .completed for diferente de nulo remove .completed,  se nao adiciona .completed
+  }
+  let adiciona = event.target.className = 'completed';
+  
+}
+//eu quero que o evento suma apos clicar duas vezes 
+listaOrdenada.addEventListener('dblclick', duploClick);
+
+function removeList() {
+  let lista = document.querySelectorAll('li')
+  if(lista !== ''){
+    listaOrdenada.remove(document.querySelector('li'))
+    
+  }
+  
+}
+
+buttonPaga.addEventListener('click', removeList)
