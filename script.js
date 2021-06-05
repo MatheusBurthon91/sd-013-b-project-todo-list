c201ecd05a04cb759c0021049c4b1f46b2ff244d
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-06-05 02:01:34 -0300

    Inicia requisito 11

--- script.js
+++ script.js
@@ -25,14 +25,16 @@ btnClearSelectioned.id = 'remover-finalizados';
 btnClearSelectioned.innerHTML = 'Limpar Completos';
 containerButtons.appendChild(btnClearSelectioned);
 
+const btnSaveTasks = document.createElement('button');
+btnSaveTasks.id = 'salvar-tarefas';
+btnSaveTasks.innerText = 'Salvar Lista';
+containerButtons.appendChild(btnSaveTasks);
+
 const ordemList = document.createElement('ol');
 ordemList.id = 'lista-tarefas';
 containerTasks.appendChild(ordemList);
 
 const corDefault = 'rgb(128, 128, 128)';
-
-// localStorage.setItem(`tarefa ${ordemList.childElementCount}.`, itemList.innerText);
-// localStorage.clear();
 
 function clearSelect() {
   const listItem = document.getElementsByClassName('item-list');
@@ -79,11 +81,43 @@ btnCreateTasks.addEventListener('click', () => {
   });
 });
 
+function createItemRestored(item, className, title, color) {
+  const itemList = document.createElement('li');
+  itemList.innerText = item;
+  itemList.className = className;
+  itemList.title = title;
+  itemList.backgroundColor = color;
+  ordemList.appendChild(itemList);
+  // itemList.addEventListener('dblclick', () => {
+  //   completed();
+  // });
+  // itemList.addEventListener('click', () => {
+  //   selectioned();
+  // });
+}
+
+function restoreList() {
+  for (let key = 0; key < ordemList.children.length * 4; key += 4) {
+    // if (localStorage.getItem(key) !== null) {
+    createItemRestored(localStorage.getItem[key],
+      localStorage.getItem[key + 1],
+      localStorage.getItem[key + 2],
+      localStorage.getItem[key + 3]);
+    // };
+  }
+}
+restoreList();
+
 btnClearAll.addEventListener('click', () => {
   const listOrden = document.querySelectorAll('.item-list');
+  const completedList = document.querySelectorAll('.completed');
   for (let index = 0; index < listOrden.length; index += 1) {
     listOrden[index].remove();
   }
+  for (let index = 0; index < completedList.length; index += 1) {
+    completedList[index].remove();
+  }
+  localStorage.clear();
 });
 
 btnClearSelectioned.addEventListener('click', () => {
@@ -92,3 +126,17 @@ btnClearSelectioned.addEventListener('click', () => {
     completeds[index].remove();
   }
 });
+
+btnSaveTasks.addEventListener('click', () => {
+  let keyItem = 0;
+  for (let index = 0; index < ordemList.children.length * 4; index += 4) {
+    localStorage.setItem([index], ordemList.children[keyItem].innerText);
+    localStorage.setItem([index + 1], ordemList.children[keyItem].className);
+    localStorage.setItem([index + 2], ordemList.children[keyItem].title);
+    localStorage.setItem([index + 3], ordemList.children[keyItem].style.backgroundColor);
+    console.log(index);
+    keyItem += 1;
+  }
+});
+
+console.log(localStorage[5]);
