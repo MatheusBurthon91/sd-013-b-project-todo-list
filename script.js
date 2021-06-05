7a92751bab812208a0400fc8355f8559121863df
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-05 17:26:13 -0300

    resolve os exercicios 7 e 8

--- script.js
+++ script.js
@@ -28,18 +28,34 @@ const listAddButton = document.createElement('button');
 listAddButton.id = 'criar-tarefa';
 listAddButton.innerText = 'Adicionar tarefa';
 divButtons.appendChild(listAddButton);
+function clickListCheck(event) {
+  const clickedItem = event.target;
+  const selectedItem = document.querySelector('.selected');
+  
+  // se não houver item selecionado, adicionar classe selected ao item clicado
+  if (selectedItem === null){
+    clickedItem.classList.add('selected');
+  } else {
+    selectedItem.classList.remove('selected');
+    clickedItem.classList.add('selected');
+  }
+  // e se ele não é nulo? tirar de onde estar e passar pro próximo clicado.
+
+}
+
 function addItem() {
   const orderList = document.querySelector('#lista-tarefas');
   const taskButton = document.querySelector('#criar-tarefa');
   // desafio cumprido com auxílio de estudos do w3Scholl, 
-  // uso de input e valores.
+  // uso de input e valores;
   taskButton.addEventListener('click', (event) => {
     const inputId = document.getElementById('texto-tarefa').value;
     const listItem = document.createElement('li');
     listItem.innerText = inputId;
+    listItem.classList = 'list-itens';
+    listItem.addEventListener('click', clickListCheck);
     orderList.appendChild(listItem);
     document.getElementById('texto-tarefa').value = '';
   });
-
 }
 addItem();
