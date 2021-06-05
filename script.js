cac8b8852d7473cd079901e90e47beedcd37d091
Jonmorais <jonathan_ms15@hotmail.com>
2021-06-04 21:28:06 -0300

    Requisito 8 + 9

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const ol = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
+let li = document.createElement('li');
 
 function createItem() {
-  let input = document.getElementById('texto-tarefa').value;
-  const li = document.createElement('li');
+  const input = document.getElementById('texto-tarefa').value;
+  li = document.createElement('li');
   ol.appendChild(li);
   li.appendChild(document.createTextNode(input));
   document.getElementById('texto-tarefa').value = '';
 }
 
 button.addEventListener('click', createItem);
+
+function changeItemColor(eventoColor) {
+  if (eventoColor.target.tagName === 'LI') {
+    const selectedListItem = document.querySelector('.selected');
+    if (selectedListItem == null) {
+      eventoColor.target.classList.add('selected');
+    } else if (selectedListItem !== eventoColor.target) {
+      selectedListItem.classList.remove('selected');
+      eventoColor.target.classList.add('selected');
+    }
+  }
+}
+
+ol.addEventListener('click', changeItemColor);
