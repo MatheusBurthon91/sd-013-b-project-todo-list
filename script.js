0aa57782e4b6eba96063e04ca175a75a3083cd1d
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-06-04 21:21:29 -0300

    modificao no script , atendendo os requisito 10 modificado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonAdicionar = document.querySelector('#criar-tarefa');
 const listaOrdenada = document.querySelector('#lista-tarefas');
-const buttonPaga= document.querySelector('#apaga-tudo');
+const buttonPaga = document.querySelector('#apaga-tudo');
 function tarefa() {
   const textInp = document.querySelector('#texto-tarefa');
   const creatLi = document.createElement('li');
@@ -12,7 +12,7 @@ function tarefa() {
 buttonAdicionar.addEventListener('click', tarefa);
 
 function colorElement(event) {
-  let classLista = document.querySelector('.lista'); 
+  let classLista = document.querySelector('.lista');
   if (classLista !== null) {
     classLista.classList.remove('lista');
   }
@@ -23,23 +23,20 @@ listaOrdenada.addEventListener('click', colorElement);
 
 function duploClick(event) {
   let classLista = document.querySelector('.completed');
-  
+
   if (classLista !== null) {
-    classLista.classList.remove('completed');     //se .completed for diferente de nulo remove .completed,  se nao adiciona .completed
+    classLista.classList.remove('completed'); //se .completed for diferente de nulo remove .completed,  se nao adiciona .completed
   }
-  let adiciona = event.target.className = 'completed';
-  
+  let adiciona = (event.target.className = 'completed');
 }
-//eu quero que o evento suma apos clicar duas vezes 
+//eu quero que o evento suma apos clicar duas vezes
 listaOrdenada.addEventListener('dblclick', duploClick);
 
 function removeList() {
-  let lista = document.querySelectorAll('li')
-  if(lista !== ''){
-    listaOrdenada.remove(document.querySelector('li'))
-    
+  let lista = document.querySelectorAll('li');
+  if (lista !== '') {
+    listaOrdenada.remove(document.querySelector('li'));
   }
-  
 }
 
-buttonPaga.addEventListener('click', removeList)
+buttonPaga.addEventListener('click', removeList);
