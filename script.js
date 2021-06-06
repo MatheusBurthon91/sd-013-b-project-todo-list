71aa70a5c3ec5509583948f937977144d4174ad8
fabio <fabiojuvenalpereira@hotmail.com>
2021-06-06 19:44:19 -0300

    Adiciona desafio 9

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const botao = document.querySelector('#criar-tarefa');
 const entradaDeTexto = document.querySelector('#texto-tarefa');
+const getClick = document.getElementById('conteudo-lista');
 
 function criaTarefa() {
   const texto = document.querySelector('#texto-tarefa').value;
@@ -8,15 +9,41 @@ function criaTarefa() {
 
 function adicionaLista() {
   const addlist = document.querySelector('#lista-tarefas');
-  const limpa = entradaDeTexto;
+  const text = entradaDeTexto;
   if (criaTarefa() !== '') {
     const lista = document.createElement('li');
     lista.innerText = criaTarefa();
+    lista.classList.add('itensLista');
     addlist.appendChild(lista);
-    limpa.value = '';
+    text.value = '';
   }
-  limpa.value = '';
+  text.value = '';
 }
+
+function removeclass() {
+  const getItem = document.querySelectorAll('.itensLista');
+  for (let indexR = 0; indexR < getItem.length; indexR += 1) {
+    getItem[indexR].classList.remove('selected');
+  }
+}
+
+getClick.addEventListener('click', (haveClick) => {
+  removeclass();
+  if (haveClick.target.classList.contains('itensLista')) {
+    haveClick.target.classList.add('selected');
+  }
+}, false);
+
+getClick.addEventListener('dblclick', (clicks) => {
+  const clicked = clicks.target;
+  if (clicked.classList.contains('itensLista')) {
+    if (clicked.classList.contains('completed')) {
+      clicked.classList.remove('completed');
+    } else {
+      clicked.classList.add('completed');
+    }
+  }
+}, false);
 
 entradaDeTexto.addEventListener('change', criaTarefa);
 botao.addEventListener('click', adicionaLista);
