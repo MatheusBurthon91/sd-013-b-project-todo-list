51bbbf37a259596d478cd10749046b68afa4f955
Letícia Castro <aicitelks@gmail.com>
2021-06-05 22:03:17 -0300

    Concluido com sucesso: Requisito 7

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 /* PROJETO 4 */
-
-/* No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. 
-
-Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
-
-A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados. */
-
-let inputTarefa = document.querySelector('#texto-tarefa');
-let btnCriarTarefa = document.querySelector('#criar-tarefa');
+const inputTarefa = document.querySelector('#texto-tarefa');
+const btnCriarTarefa = document.querySelector('#criar-tarefa');
 
 function manipularTarefas() {
-  // texto do imput
-  let textoTarefa = document.querySelector('#texto-tarefa').value;
-  let olTarefas = document.querySelector('#lista-tarefas');
-  let liTarefa = document.createElement('li');
+  /* texto do imput */
+  const textoTarefa = document.querySelector('#texto-tarefa').value;
+  const olTarefas = document.querySelector('#lista-tarefas');
+  const liTarefa = document.createElement('li');
+
   liTarefa.innerText = textoTarefa;
-
   olTarefas.appendChild(liTarefa);
-
   inputTarefa.value = '';
 }
+btnCriarTarefa.addEventListener('click', manipularTarefas);
 
-  btnCriarTarefa.addEventListener('click', manipularTarefas);
+function selecionarItem(event){
+  let itemClicado = event.target;
+  itemClicado.classList.toggle('selecionado');
+}
+let lista = document.querySelector('#lista-tarefas');
+lista.addEventListener('click', selecionarItem);
