83aa7978759c40c6aca3b9740608bccfd3685163
Letícia Castro <aicitelks@gmail.com>
2021-06-05 22:34:00 -0300

    Concluido com sucesso: Requisito 8

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 const inputTarefa = document.querySelector('#texto-tarefa');
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 
+// * PAI da lista 'ol'
+let olTarefas = document.querySelector('#lista-tarefas');
+
+// * FILHOS  da lista 'li'
+let filhosTarefas = olTarefas.children;
+
 function manipularTarefas() {
   /* texto do imput */
   const textoTarefa = document.querySelector('#texto-tarefa').value;
-  const olTarefas = document.querySelector('#lista-tarefas');
+  //const olTarefas = document.querySelector('#lista-tarefas');
   const liTarefa = document.createElement('li');
 
   liTarefa.innerText = textoTarefa;
@@ -16,7 +22,13 @@ btnCriarTarefa.addEventListener('click', manipularTarefas);
 
 function selecionarItem(event){
   let itemClicado = event.target;
-  itemClicado.classList.toggle('selecionado');
+  itemClicado.classList.add('selecionado');
+
+  /* * remover a classe 'selecionado' do irmão */
+  for (let i = 0; i < filhosTarefas.length; i += 1){
+    if (filhosTarefas[i] !== itemClicado) {
+      filhosTarefas[i].classList.remove('selecionado');
+    }
+  }
 }
-let lista = document.querySelector('#lista-tarefas');
-lista.addEventListener('click', selecionarItem);
+olTarefas.addEventListener('click', selecionarItem);
