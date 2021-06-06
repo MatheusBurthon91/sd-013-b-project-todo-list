0a90f2e4caf55c1e784105665c67fe38d25ac5c5
Pedro Brito <pedroab0119@gmail.com>
2021-06-06 13:33:21 -0300

    Add pTagWorking and style it

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const body = document.querySelector('body');
 const headerTag = document.createElement('header');
 headerTag.innerText = 'Minha Lista de Tarefas';
 body.appendChild(headerTag);
+
+const pTagWorking = document.createElement('p');
+pTagWorking.id = 'funcionamento';
+pTagWorking.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+body.appendChild(pTagWorking);
