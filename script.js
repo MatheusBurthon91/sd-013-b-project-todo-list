eccc09d73dbd7c21d9a0cfaf26168d2a238a1765
r0ger <rogeriomaiajr13@gmail.com>
2021-06-07 16:13:10 -0300

    function para o select item criada

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ const buttonTaskCreated = document.querySelector('#criar-tarefa');
 const txtInput = document.querySelector('#texto-tarefa');
 const tasksList = document.querySelector('#lista-tarefas');
 
-function clearInputText() {
-  txtInput.value = '';
-}
-
 function createTask(task, classOfTask) {
   const listItem = document.createElement('li');
   listItem.innerText = task;
   listItem.className = classOfTask;
   tasksList.appendChild(listItem);
-  clearInputText(); // call the function to clear the input
+  txtInput.value = ''; // call the function to clear the input
 }
 
 function createTaskBtn() {
@@ -20,4 +16,19 @@ function createTaskBtn() {
   createTask(task, classOfTask);
 }
 
+let allTasks = tasksList.children;
+console.log(allTasks);
+
+function selectItem(event) {
+  const clickItem = event.target; // pega o evento click
+  clickItem.classList.add('selecionado'); // add classe selecionado
+
+  for (let index = 0; index < allTasks.length; index += 1) {
+    if (allTasks[index] !== clickItem) {
+      allTasks[index].classList.remove('selecionado');
+    }
+  }
+}
+
 buttonTaskCreated.addEventListener('click', createTaskBtn);
+allTasks.addEventListener('click', selectItem);
