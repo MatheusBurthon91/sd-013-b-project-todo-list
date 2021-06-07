3c293bbfe45b6447f335812ce7d8569e261a2348
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-06-07 15:43:18 -0300

    Requisitos 5 e 6 concluidos

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+
+function newItem() {
+  let task = document.getElementById("texto-tarefa");
+  let tasklist = document.getElementById("lista-tarefas");
+  let newTask = document.createElement("li");
+
+  newTask.innerText = task.value;
+  tasklist.appendChild(newTask);
+  task.value = "";
+}
+
+/* let tasklist = document.getElementById("lista-tarefas");
+let tarefa1 = document.createElement("li");
+tarefa1.innerText = "Concluir o projeto 2";
+tasklist.appendChild(tarefa1); */
