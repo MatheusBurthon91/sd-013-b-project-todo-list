5378b8b4838d02533a3e00a8f4607dd8b66c6bfd
JoãoBDf <joaobdf@hotmail.com>
2021-06-07 17:01:40 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const buttonText = document.getElementById('criar-tarefa');
+const listItem = document.getElementsByTagName('li');
 
 function clickList() {
   const mainList = document.getElementById('lista-tarefas');
   const text = document.getElementById('texto-tarefa');
   const list = document.createElement('li');
-  console.log(text.value)
   list.innerText = text.value;
   mainList.appendChild(list);
   text.value = '';
+  mainList.lastElementChild.addEventListener('click', itemSelect);
 }
 
 buttonText.addEventListener('click', clickList);
+
+function itemSelect(event) {
+    const item = event.target;
+    const select = document.getElementsByClassName('itemList');
+    if (item.classList.contains('itemList')) {
+        //
+    } else if (select.length === 0) {
+        item.classList.add('itemList');
+    }
+}
