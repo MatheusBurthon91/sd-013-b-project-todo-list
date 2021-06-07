1e72e22069e18e5ee875f99b6fa281b0f9bf2aca
JoãoBDf <joaobdf@hotmail.com>
2021-06-07 16:01:19 -0300

    Ue ja foi o 6 =D

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const buttonText = document.getElementById('criar-tarefa');
 
 function clickList() {
-    const mainList = document.getElementById('lista-tarefas');
-    const text = document.getElementById('texto-tarefa');
-    const list = document.createElement('li');
-    console.log(text.value)
-    list.innerText = text.value;
-    mainList.appendChild(list)
-    text.value = '';
+  const mainList = document.getElementById('lista-tarefas');
+  const text = document.getElementById('texto-tarefa');
+  const list = document.createElement('li');
+  console.log(text.value)
+  list.innerText = text.value;
+  mainList.appendChild(list);
+  text.value = '';
 }
 
 buttonText.addEventListener('click', clickList);
