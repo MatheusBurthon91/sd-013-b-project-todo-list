2a4ff2ad1914fe40891951c0a68d3d3341e30c9b
José Breno <brenojbs@hotmail.com>
2021-06-07 19:22:41 -0300

    desafio 5 completo?

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+//Cria a li em ordem
 document.querySelector('#criar-tarefa').addEventListener('click', function () {
   let novaT = document.createElement('li');
   let ol = document.querySelector('#lista-tarefas');
   let input = document.querySelector('#texto-tarefa').value;
   ol.appendChild(novaT).innerHTML = input;
+
   //Consegui chegar a esta solução usando a ideia do César Sousa nesta pagina  https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
   document.querySelector('#texto-tarefa').value = '';
+
+  novaT.addEventListener('click', function selecionar() {
+    novaT.classList.toggle('selected')
+    })  
 })
+
+
+// Complicado Preciso retirar uma classe
+  
