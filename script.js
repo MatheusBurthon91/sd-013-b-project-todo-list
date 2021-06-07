31ba7c0dc56431c3090fea2db5352498e0239f94
Camila Dornas <camiladornas@gmail.com>
2021-06-07 20:25:17 -0300

    Add requisitos 1 ao 8

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function addTarefa() {
       tarefa.innerText = box.value;
       tarefa.className = 'lista';
       tarefa.addEventListener('click', coloreTarefa);
+      tarefa.addEventListener('dblclick', riscaTarefa);
       const listaOrdenada = document.querySelector('#lista-tarefas');
       listaOrdenada.appendChild(tarefa);
       box.value = "";
@@ -28,8 +29,8 @@ addTarefa();
 // -------> Questão 7 <------- //
 // -------> Questão 8 <------- //
 
-function coloreTarefa(event) {
-  const tarefa = event.target;
+function coloreTarefa(clicado) {
+  const tarefa = clicado.target;
   const selecionado = document.querySelector('.selected');
   if (selecionado === null) {
     tarefa.classList.add('selected');
@@ -39,4 +40,40 @@ function coloreTarefa(event) {
     tarefa.classList.add('selected');
   };
 };
-coloreTarefa();
+
+// -------> Questão 9 <------- //
+
+function riscaTarefa (event) {
+    event.target.classList.toggle('completed')
+};
+
+// -------> Questão 10 <------- //
+// -------> Questão 11 <------- //
+
+// Cria botões //
+
+function createButton(nome, id) {
+  const position = document.querySelector('#body');
+  const botaoClear = document.createElement('button');
+  botaoClear.id = id;
+  botaoClear.innerText = nome;
+  position.appendChild(botaoClear);
+  
+}
+createButton('Limpar lista', 'apaga-tudo');
+createButton('Limpar tarefas realizadas', 'remover-finalizados');
+
+  
+// limpa a lista //
+function clear() {
+  let botaoClear = document.querySelector('#apaga-tudo');
+  botaoClear.addEventListener('click', function() {
+    const listaOrdenada = document.getElementsByTagName('ol');
+    const itens = document.querySelectorAll('li');
+    if(listaOrdenada.Children !== null) {
+      listaOrdenada.removeChildren(itens);
+    }
+  });
+};
+  
+  clear();
