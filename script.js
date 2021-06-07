170fa6ed451aff96a58c4c4ccddefeb8de91e583
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-06-07 18:35:45 -0300

    requisito 7, 8, 9, 10 e 11

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.onload = function(){
 }
+const itemList = document.querySelector('#lista-tarefas');
 
 function addItem(){ 
     const parentElement = document.getElementById("lista-tarefas");
     let input = document.getElementById('texto-tarefa').value;
     let newItem = document.createElement('li');
     newItem.innerHTML = input;
+		newItem.className = 'item';
 		// thread do stackOverFlow me ajudou a parar a função addItem caso a condição fosse atendida: https://stackoverflow.com/questions/3536055/stopping-a-javascript-function-when-a-certain-condition-is-met;
 		if (input === '') {
 			alert('Impossível adicionar item vazio à lista');
@@ -13,25 +15,24 @@ function addItem(){
 		}
     parentElement.appendChild(newItem);
     document.querySelector('input').value = '';
-		localStorage.setItem("item", newItem.innerHTML);
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", addItem);
 
-const itemList =  document.querySelector('#lista-tarefas');
-itemList.addEventListener("click", function (e) {
-    for (let index = 0; index < itemList.children.length; index += 1) {
-     itemList.children[index].classList.toggle('selected');
-		 console.log(e.target);
-    }
-});
+itemList.addEventListener("click", (e) => {
+	const selectedItem = document.querySelector('.selected');
+	if (selectedItem != null){
+		selectedItem.classList.remove('selected');
+	} e.target.classList.add('selected');
+})
 
-const itemListCompleted =  document.querySelector('#lista-tarefas');
-itemListCompleted.addEventListener("dblclick", function () {
-    for (let index = 0; index < itemListCompleted.children.length; index += 1) {
-     itemListCompleted.children[index].classList.toggle('completed');
-    }
+
+itemList.addEventListener("dblclick", (e) => {
+    const completedItem = document.querySelectorAll('.completed');
+		if(completedItem != null){
+			e.target.classList.toggle('completed')
+		} 
 })
 
 const deleteEveryt = document.querySelector('#apaga-tudo');
@@ -48,7 +49,6 @@ function removeCompleted () {
 	const finished = document.querySelectorAll('.completed');
 	for (let index = 0; index < finished.length; index += 1){
 	finished[index].remove();
-	return removeCompleted();
 	}
 }
 
