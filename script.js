c2b5e0b3966bf6070dab0dd5e841fe73eb7417a2
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-06-06 20:54:12 -0400

    selecao cinza

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 let texto = '';
 let temp = ''
-
+let lista = document.querySelector('#lista-tarefas');
 
 function capturar(){
 texto = document.getElementById('texto-tarefa').value;
 temp = texto;
 
-let lista = document.querySelector('#lista-tarefas');
+
 novoItem = document.createElement('li');
 novoItem.innerHTML = temp;
 lista.appendChild(novoItem);
 document.getElementById('texto-tarefa').value = '';
 }
 
+function limpaCinza(){
+    item = lista.children;
+    console.log(item[0]);
+    for (index = 0; index < item.length; index += 1){
+        item[index].style.background = 'rgb(255,255,255)';
+    }
+}
 
+function paint(event){
+    limpaCinza();
+    let x = event.target
+    console.log(x);
+    x.style.background = 'rgb(128,128,128)';
+}
 
 
 
