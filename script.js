a3126f8caa0cb6854897cbb6e4be777f46c6ebf0
thiagoteixas <thiagoteixas@gmail.com>
2021-06-07 14:31:33 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function createTask () {
   taskList.appendChild(task);
 }
 
-function changeBackgroundColor (event) {
+function changeBackgroundColor(event) {
+  const selected = document.getElementsByClassName('selected');
+  if (selected.length === 1) {
+    selected[0].style.backgroundColor = '';
+    selected[0].classList.remove('selected');
+  }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.classList.add('selected');
 }
 
-let createTaskButton = document.getElementById('criar-tarefa');
+const createTaskButton = document.getElementById('criar-tarefa');
 let taskToBeCreated = document.getElementById('texto-tarefa');
 let taskList = document.getElementById('lista-tarefas');
 
