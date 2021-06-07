ad99e134e381a007ba9b4bf77949227fb8ebec87
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-06-07 16:12:04 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const taskList = document.querySelector('#lista-tarefas');
 const taskButton = document.querySelector('#criar-tarefa');
 const taskInput = document.getElementById('texto-tarefa');
+let listItem;
 
 function listMaker() {
-  let inputText = taskInput.value;
-  let listItem = document.createElement('li');
+  const inputText = taskInput.value;
+  listItem = document.createElement('li');
   listItem.classList.add('task');
   listItem.innerText = inputText;
   taskList.appendChild(listItem);
@@ -12,3 +13,18 @@ function listMaker() {
 }
 
 taskButton.addEventListener('click', listMaker);
+
+function selectedItem(event) {
+  const liArray = document.getElementsByTagName('li');
+  for (let index = 0; index < liArray.length; index += 1) {
+    if (event.target.classList.contains('task')) {
+      if (liArray[index].classList.contains('selected')) {
+        liArray[index].classList.remove('selected');
+      } else {
+        event.target.classList.add('selected');
+      }
+    }
+  }
+}
+
+taskList.addEventListener('click', selectedItem);
