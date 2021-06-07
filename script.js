9457fb2013a52ae395339b42c05e5929e086d19c
AquilesDantas <aquilesdant@gmail.com>
2021-06-06 21:23:13 -0300

    Realizacao do desafio 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //Salva as tarefas na lista ordenada e limpa o imput//
 function tarefas() {
     let input = document.getElementById("texto-tarefa").value;
@@ -6,6 +7,13 @@ function tarefas() {
     let itemLista = document.createElement("li");
     lista.appendChild(itemLista);
     itemLista.innerText = input;
+    itemLista.addEventListener("click", addCinza);
     document.getElementById("texto-tarefa").value = '';
     
     }
+
+    function addCinza (event) {
+        event.target.classList.add("cinza");
+        }
+        
+
