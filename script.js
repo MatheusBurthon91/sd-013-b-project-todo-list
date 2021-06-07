a755656fd70ff0cdef336f2028ba5a27d2e2d0cf
fabio <fabiojuvenalpereira@hotmail.com>
2021-06-07 14:45:51 -0300

    corrige erro do desafio 10

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const entradaDeTexto = document.querySelector('#texto-tarefa');
 const getClick = document.getElementById('conteudo-lista');
 const clearAll = document.querySelector('#apaga-tudo');
 const rmIndividual = document.querySelector('#remover-finalizados');
-const getItensLista = document.querySelectorAll('.itensLista');
+
 function criaTarefa() {
   const texto = document.querySelector('#texto-tarefa').value;
   return texto;
@@ -23,7 +23,7 @@ function adicionaLista() {
 }
 
 function removeclass() {
-  const getItem = getItensLista;
+  const getItem = document.querySelectorAll('.itensLista');
   for (let indexR = 0; indexR < getItem.length; indexR += 1) {
     getItem[indexR].classList.remove('selected');
   }
@@ -48,14 +48,14 @@ getClick.addEventListener('dblclick', (clicks) => {
 }, false);
 
 clearAll.addEventListener('click', () => {
-  const listToRm = getItensLista;
+  const listToRm = document.querySelectorAll('.itensLista');
   for (let starts = 0; starts < listToRm.length; starts += 1) {
     listToRm[starts].parentNode.removeChild(listToRm[starts]);
   }
 });
 
 function remComplete() {
-  const remove = getItensLista;
+  const remove = document.querySelectorAll('.itensLista');
   for (let index = 0; index < remove.length; index += 1) {
     const rm = remove[index];
     if (rm.classList.contains('completed')) {
