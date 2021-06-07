aed96f332b9a312b71dee20fc9a9271aa29a8a4e
Bruno <brunodevmoraes@gmail.com>
2021-06-07 18:34:42 -0300

    Adiciona requistos 7 e 8

--- script.js
+++ script.js
@@ -4,15 +4,32 @@ let button = document.getElementById("criar-tarefa");
 
 button.addEventListener("click", addTask);
 
+let itemCounter = 0;
+
 function addTask(){
     let newLi = document.createElement("li");
     let input = document.getElementById("texto-tarefa").value;
     let text = document.createTextNode(input);
     newLi.appendChild(text);
     list.appendChild(newLi);
-    document.getElementById("texto-tarefa").value = "";
 
-    console.log(input); //Pq é preciso usar o createTextNode 
-}                       //para conseguir fazer o appendChild?
-                        //Tendo em vista que o input já é um
-                        //texto como pode ser visto no console.log.
+    newLi.addEventListener("click", changeColor);
+    document.getElementById("texto-tarefa").value = "";
+   
+    function changeColor(){
+        if(document.querySelectorAll(".grey").length !== 0){
+        document.querySelector(".grey").style.backgroundColor = document.body.style.backgroundColor;
+        document.querySelector(".grey").classList.remove("grey");    
+        event.target.style.backgroundColor = "rgb(128, 128, 128"     
+        event.target.classList.add("grey");
+        }
+            else{
+                event.target.style.backgroundColor = "rgb(128, 128, 128"     
+                event.target.classList.add("grey");
+            }
+
+    } 
+
+} 
+
+
