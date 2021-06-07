3513c3af207be595f7db0bc745d0e4ed3563ef09
Alex <alexcorreas1994@gmail.com>
2021-06-07 16:38:35 -0300

    oitavo requisito feito

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const pegarCriarTarefas = document.querySelector('#criar-tarefa');
 pegarCriarTarefas.addEventListener('click', adicionarTarefas);
 
 function oneSelected(event) {
-  const selectedElement = document.querySelector('.selected');
-  selectedElement.classList.remove('selected');
-  event.target.classList.add('selected');
+  const newList = document.querySelector('.selected');
+  if (newList === null) {
+    event.target.classList.toggle('selected');
+  } else {
+    newList.classList.remove('selected');
+    newList.style.backgroundColor = '';
+    event.target.classList.toggle('selected');
+  }
 }
-const pegarListaTarefa = document.querySelector('#lista-tarefas');
-pegarListaTarefa.addEventListener('click', (event) => {
-  const evento = event;
-  evento.target.style.background = 'rgb(128, 128, 128)';
-});
-const novaLista = document.createElement('li');
-novaLista.addEventListener('click', oneSelected);
+const list = document.querySelector('#lista-tarefas');
+list.addEventListener('click', oneSelected);
