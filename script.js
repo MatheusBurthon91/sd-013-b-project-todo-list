521f83bd79296cbfc9768150b6a2c5ba62c4d3c9
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-06-07 14:49:44 -0300

    Adiciona requisito 12

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const ol = document.getElementById('lista-tarefas');
 // Adicionar as tarefas na lista
 function addTask() {
   button.addEventListener('click', function () {
-    const liCreate = document.createElement('li');
-    ol.appendChild(liCreate);
-    liCreate.innerText = input.value;
-    input.value = '';
+    if (input.value === '') {
+      alert('Adicione uma tarefa!!!');
+    } else {
+      const liCreate = document.createElement('li');
+      ol.appendChild(liCreate);
+      liCreate.innerText = input.value;
+      input.value = '';
+    }
   });
 }
-
 addTask();
 
 // Mudar a cor do background ao clicar em um item da lista
@@ -66,3 +69,29 @@ function removeTasksFinished() {
   });
 }
 removeTasksFinished();
+
+// Botão para salvar tarefas no localStorage
+const buttonSaveTasks = document.getElementById('salvar-tarefas');
+
+function saveTasks() {
+  buttonSaveTasks.addEventListener('click', function () {
+    let cont = 0;
+    for (let index = 0; index < li.length; index += 1) {
+      if (li[index].className.includes('completed')) {
+        cont += 1;
+      }
+    }
+    if (cont >= 1) {
+      localStorage.setItem('task', ol.innerHTML);
+    }
+  });
+}
+saveTasks();
+
+// Pegar o resultado da função anterior e deixar visível no browser
+window.onload = function showSalvedDatas() {
+  const importSalvedTasks = localStorage.getItem('task');
+  if (importSalvedTasks) {
+    ol.innerHTML = importSalvedTasks;
+  }
+};
