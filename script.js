6ea8287f20df8dc41fd116b101829c6872f2a8d0
bvottri <bruna.votre@gmail.com>
2021-06-06 23:10:06 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let h1 = document.createElement('h1');
-h1.innerHTML = 'Minha Lista de Tarefas'
+h1.innerHTML = 'Minha Lista de Tarefas';
+
+let paragraph = document.createElement('p')
+paragraph.id = 'funcionamento';
+paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 
 let header = document.createElement('header');
 header.appendChild(h1);
+header.appendChild(paragraph);
 
 let body = document.getElementsByTagName('body')[0];
-body.appendChild(header)
+body.appendChild(header);
