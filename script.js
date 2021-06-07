61f8b7d757e0fff30203f113bcc896221c79aeac
bvottri <bruna.votre@gmail.com>
2021-06-07 15:03:46 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//HTML
 let h1 = document.createElement('h1');
 h1.innerHTML = 'Minha Lista de Tarefas';
 
@@ -9,7 +10,11 @@ let header = document.createElement('header');
 header.appendChild(h1);
 header.appendChild(paragraph);
 
-let input = document.createElement('input')
+let button = document.createElement('button');
+button.id = 'criar-tarefa'
+button.innerHTML = 'Adicionar Tarefa'
+
+let input = document.createElement('input');
 input.id = 'texto-tarefa';
 
 let list = document.createElement('ol');
@@ -18,4 +23,21 @@ list.id = 'lista-tarefas';
 let body = document.getElementsByTagName('body')[0];
 body.appendChild(header);
 body.appendChild(input);
-body.appendChild(list)
+body.appendChild(list);
+body.appendChild(button);
+
+//Clicar no botão = Adicionar Texto do Input e limar Input
+
+function addTask() {
+    button.addEventListener('click', function() {
+        let li = document.createElement('li');
+        li.innerText = input.value;
+        list.appendChild(li);
+        input.value = '';
+    })
+}
+addTask()
+
+function clearInput() {
+
+}
