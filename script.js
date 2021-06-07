1aee0d8c88df39aae85a4ef63153296b98e833a8
José Breno <brenojbs@hotmail.com>
2021-06-07 08:49:39 -0300

    desafio 5 tentando.....

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.querySelector('#criar-tarefa').addEventListener('click', function () {
   let novaT = document.createElement('li');
   let ol = document.querySelector('#lista-tarefas');
-  let input = document.querySelector('#texto-tarefa').value
-  ol.appendChild(novaT).innerHTML = input
-  document.querySelector('#texto-tarefa').reset()
+  let input = document.querySelector('#texto-tarefa').value;
+  ol.appendChild(novaT).innerHTML = input;
+  //Consegui chegar a esta solução usando a ideia do César Sousa nesta pagina  https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
+  document.querySelector('#texto-tarefa').value = '';
 })
