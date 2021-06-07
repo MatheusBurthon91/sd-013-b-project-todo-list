a99b043b48fcc0911d064a6b38d12340d6197379
Miguel <migmenezes.mm2@gmail.com>
2021-06-07 16:13:36 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function atributos(){
-    const list = document.querySelector('#lista-tarefas');
-    const text = document.querySelector('#texto-tarefa').value;
-    const li = document.createElement('li');
-    document.querySelector('#texto-tarefa').value = null;
-  
-    li.innerText= text;
-list.appendChild(li);
-}
 const botao = document.querySelector('#criar-tarefa');
-botao.addEventListener("click",atributos)
+botao.addEventListener("click",elementos)
+ function elementos(){
+    let li = document.createElement('li');
+    let list = document.querySelector('#lista-tarefas');
+    let text = document.querySelector('#texto-tarefa').value;
+    document.querySelector('#texto-tarefa').value = null;
+    
+    
+    li.innerText = text;
+    list.appendChild(li);
+    li.addEventListener('click',function(event){
+        
+        event.target.style.backgroundColor = 'grey'
+    });
+}
+
+
+
+
+function selecionarItem(event){
+
+}
+
+
 
