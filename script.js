046bbb7f811c0ab3e9e7be114351508fc6d74eb2
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-07 15:49:12 -0300

    Corrige linhas conforme lint

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ function createTask() {
   const button = document.getElementById('criar-tarefa');
   const input = document.getElementById('texto-tarefa');
   const list = document.getElementById('lista-tarefas');
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function () {
     if (input.value.length > 0) {
-        const listItem = document.createElement('li');
-        listItem.innerText = input.value;
-        
-        list.appendChild(listItem); 
-        input.value = '';
+      const listItem = document.createElement('li');
+      listItem.innerText = input.value;
+      list.appendChild(listItem); 
+      input.value = '';
     } else {
-        alert('Erro: digite ao menos 1 caractere');
+      alert('Erro: digite ao menos 1 caractere');
     }
   })
-}
+};
 
 createTask();
