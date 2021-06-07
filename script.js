ced9b9f8b7b399e20f4f842044f06e028224d377
Thiago <thiagodesouza.dev@gmail.com>
2021-06-07 18:18:57 -0300

    limpar tudo e seletor de item

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-const input = document.querySelector('#texto-tarefa');
-const createTask = document.querySelector('#criar-tarefa');
-const listTasks = document.querySelector('#lista-tarefas');
+const textoTarefa = document.querySelector('#texto-tarefa');
+const criarTarefas = document.querySelector('#criar-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
 
-createTask.addEventListener('click', () => {
+criarTarefas.addEventListener('click', () => {
   const newLi = document.createElement('li');
-  newLi.innerText = input.value;
-  listTasks.appendChild(newLi);
-  input.value = '';
+  newLi.innerText = textoTarefa.value;
+  listaTarefas.appendChild(newLi);
+  textoTarefa.value = '';
 });
+
+function seletorItem() {
+  listaTarefas.addEventListener('click', (event) => {
+    const colorGrey = 'rgb(128, 128, 128)';
+    const li = document.querySelectorAll('li');
+    for (let index = 0; index < li.length; index += 1) {
+      if (li[index].style.backgroundColor === colorGrey) {
+        li[index].style.backgroundColor = '';
+        li[index].classList.remove('seletorColor');
+      }
+    }
+    const evt = event.target;
+    evt.style.backgroundColor = colorGrey;
+    evt.classList.add('seletorColor');
+  });
+}
+seletorItem();
+
+function clearTasks() {
+  const clearAllTasks = document.querySelector('#apaga-tudo');
+  clearAllTasks.addEventListener('click', () => {
+
+    listaTarefas.innerText = '';
+
+  });
+}
+clearTasks();
+
