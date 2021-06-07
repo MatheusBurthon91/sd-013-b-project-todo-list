da5420770481802b01f2d923ce79a39bfda4d66c
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-06-07 16:17:51 -0300

    Desafio 9 completo e alterações no css e html

--- script.js
+++ script.js
@@ -9,14 +9,36 @@ function createElement(nameElement, idElementPai) {
 
 function limparMarcações() {
   for (let index = 0; index < elementListItem.length; index += 1) {
-    elementListItem[index].style.backgroundColor = 'white';
+    if (elementListItem[index].style.backgroundColor === 'red') {
+      // Não faça nada
+    } else {
+      elementListItem[index].style.backgroundColor = 'white';
+    }
   }
 }
 
 function changeBackground(event) {
   limparMarcações();
   const eventTarget = event.target;
-  eventTarget.style.backgroundColor = 'rgb(128,128,128)';
+  if (eventTarget.style.backgroundColor === 'red') {
+    // Não faça nada
+  } else {
+    eventTarget.style.backgroundColor = 'rgb(128,128,128)';
+  }
+}
+
+function todoCompleted(event) {
+  const eventTarget = event.target;
+  if (eventTarget.style.backgroundColor !== 'red') {
+    // Se a cor do elemento 'li' da lista for difente de vermelho:
+    // Torna ele vermelho
+    eventTarget.className = 'completed';
+    eventTarget.style.backgroundColor = 'red';
+  } else if (eventTarget.style.backgroundColor === 'red') {
+    // Se ele já for vermelho: torna a ficar branco
+    eventTarget.className = '';
+    eventTarget.style.backgroundColor = 'white';
+  }
 }
 
 function adicionaNaLista() {
@@ -27,7 +49,9 @@ function adicionaNaLista() {
     createElement('li', 'lista-tarefas');
     // Adiciona o texto do input no 'li' do HTML
     elementListItem[contador].innerHTML = inputValue;
+    // Adiciona evento de marcação no 'li'
     elementListItem[contador].addEventListener('click', changeBackground);
+    elementListItem[contador].addEventListener('dblclick', todoCompleted);
     // Reseta o texto do input e adiciona 1 no contador
     document.getElementById('texto-tarefa').value = '';
     contador += 1;
