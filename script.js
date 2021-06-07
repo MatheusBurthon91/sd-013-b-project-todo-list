39933837ebb03bf12968d8717ffaae46173251cb
Miguel <migmenezes.mm2@gmail.com>
2021-06-07 16:50:47 -0300

    text decoration

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const botao = document.querySelector('#criar-tarefa');
-botao.addEventListener("click",elementos)
- function elementos(){
+botao.addEventListener("click", elementos)
+function elementos() {
     let li = document.createElement('li');
     let list = document.querySelector('#lista-tarefas');
     let text = document.querySelector('#texto-tarefa').value;
     document.querySelector('#texto-tarefa').value = null;
-    
-    
+
+
     li.innerText = text;
     list.appendChild(li);
-    li.addEventListener('click',function(event){
+    li.addEventListener('click', function (event) {
+
+        event.target.style.backgroundColor = 'grey';
         
-        event.target.style.backgroundColor = 'grey'
-    });
+    }); 
+    li.addEventListener('dblclick',function(event){
+        event.target.classList.add('completed');
+       
+        event.target.style.textDecoration = 'line-through solid rgb(0,0,0)';
+    })
+  
+  
 }
 
 
 
 
-function selecionarItem(event){
+function selecionarItem(event) {
 
 }
 
