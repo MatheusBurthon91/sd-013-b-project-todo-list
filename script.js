1f72c331f865f1689c3933309c7b13333b5cda3e
Guilherme <guilhermerg1234@gmail.com>
2021-06-06 23:02:39 -0300

    Ex 7 e 8

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ function addTask() {
     let valueInput = document.querySelector('#texto-tarefa');
     let valueLi = document.createElement('li');
     valueLi.innerHTML = valueInput.value;
+    valueLi.classList.add('tarefas')
     document.getElementById('lista-tarefas').appendChild(valueLi);
     valueInput.value = '';
 }
 
+let olGrey = document.querySelector('#lista-tarefas');
+let liGrey = document.getElementsByTagName('li');
+olGrey.addEventListener('click', (event) => {
+    if (event.target.classList.contains('tarefas')) {
+        for (let index = 0; index < liGrey.length; index += 1) {
+            if (liGrey[index].classList.contains('grey')) {
+                    liGrey[index].classList.remove('grey')
+            }
+        }
+    }
+    event.target.classList.add('grey')
+});
