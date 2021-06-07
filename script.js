33b25a81310fa9c4d8c90b8ca733e5b4a37a633a
fabio <fabiojuvenalpereira@hotmail.com>
2021-06-07 16:17:08 -0300

    Atualiza o desafio 11 e refatora as funções que eram repetidas

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const getClick = document.getElementById('conteudo-lista');
 const clearAll = document.querySelector('#apaga-tudo');
 const rmIndividual = document.querySelector('#remover-finalizados');
 
+// Resgata o que foi escrito na caixa de texto
 function criaTarefa() {
   const texto = document.querySelector('#texto-tarefa').value;
   return texto;
 }
 
+// Adiciona o Item na lista.
 function adicionaLista() {
   const addlist = document.querySelector('#lista-tarefas');
   const text = entradaDeTexto;
@@ -22,13 +24,15 @@ function adicionaLista() {
   text.value = '';
 }
 
+// Remove os itens selecionados
 function removeclass() {
-  const getItem = document.querySelectorAll('.itensLista');
-  for (let indexR = 0; indexR < getItem.length; indexR += 1) {
-    getItem[indexR].classList.remove('selected');
+  const getSelected = document.querySelector('.selected');
+  if (getSelected != null) {
+    getSelected.classList.remove('selected');
   }
 }
 
+// Seleciona os itens pelo click do mouse
 getClick.addEventListener('click', (haveClick) => {
   removeclass();
   if (haveClick.target.classList.contains('itensLista')) {
@@ -36,6 +40,7 @@ getClick.addEventListener('click', (haveClick) => {
   }
 }, false);
 
+// Marca como completo o item que foi clicado duas vezes
 getClick.addEventListener('dblclick', (clicks) => {
   const clicked = clicks.target;
   if (clicked.classList.contains('itensLista')) {
@@ -47,23 +52,27 @@ getClick.addEventListener('dblclick', (clicks) => {
   }
 }, false);
 
+// limpa toda a lista de tarefas
 clearAll.addEventListener('click', () => {
   const listToRm = document.querySelectorAll('.itensLista');
-  for (let starts = 0; starts < listToRm.length; starts += 1) {
-    listToRm[starts].parentNode.removeChild(listToRm[starts]);
+  for (let index = 0; index < listToRm.length; index += 1) {
+    listToRm[index].parentNode.removeChild(listToRm[index]);
   }
 });
 
+// remove os itens que estão marcados como feitos
 function remComplete() {
-  const remove = document.querySelectorAll('.itensLista');
+  const remove = document.querySelectorAll('.completed');
   for (let index = 0; index < remove.length; index += 1) {
     const rm = remove[index];
-    if (rm.classList.contains('completed')) {
-      rm.parentNode.removeChild(rm);
-    }
+    rm.parentNode.removeChild(rm);
   }
 }
 
 rmIndividual.addEventListener('click', remComplete);
 entradaDeTexto.addEventListener('change', criaTarefa);
 botao.addEventListener('click', adicionaLista);
+// referências
+// A referência para criar a função 'clearAll.addEventListener' foi retirada deste site https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
+// E também https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+// Houveram algumas refatorações de partes do código, e estas mesmas foram elaboradas durante os plantões. Agradecimentos ao Panta e Baêta!
