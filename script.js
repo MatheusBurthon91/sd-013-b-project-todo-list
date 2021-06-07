4562ea0ebcc59be2f19c0937f61f4b2400d94911
Natalia Camarinha <natcamarinha@gmail.com>
2021-06-07 14:53:57 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function createTask() {
-  const taskList = document.getElementById('lista-tarefas'); 
-  // const addTask = document.getElementById('criar-tarefa');
+  const taskList = document.getElementById('lista-tarefas');
   const taskLine = document.createElement('li');
   taskLine.innerText = document.getElementById('texto-tarefa').value;
   taskLine.className = 'tarefas';
   taskList.appendChild(taskLine);
   document.getElementById('texto-tarefa').value = '';
-} 
+}
 document.getElementById('criar-tarefa').addEventListener('click', createTask);
+
+const list = document.getElementById('lista-tarefas');
+
+function changeColor(event) {
+  const tasks = document.querySelectorAll('li');
+
+  for (let index = 0; index < tasks.length; index += 1) {
+
+    if (tasks[index] === event.target) {
+      event.target.classList.add('selected');
+    } else {
+      tasks[index].classList.remove('selected');
+    }
+  }
+}
+list.addEventListener('click', changeColor);
