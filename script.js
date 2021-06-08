98f02a96d817f1eced779fa222f2cdca50e2d488
Guilherme Viana <guilhermehviana01@gmail.com>
2021-06-08 05:49:21 -0300

    Adiciona parágrafo abaixo do título

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ title.innerText = 'Minha Lista de Tarefas';
 
 header.appendChild(title);
 
+// Adicionar um pequeno parágrafo abaixo do título com id=funcionamento
+const p = document.createElement('p');
+p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+p.id = 'funcionamento';
+
+title.appendChild('p');
