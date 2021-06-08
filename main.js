a0201b7abebceced0a484f81c093713aca025391
Guilherme Viana <guilhermehviana01@gmail.com>
2021-06-08 06:21:52 -0300

    Adiciona parágrafo abaixo do título

---
+++ main.js
@@ -0,0 +1,23 @@
+// Adicionar título "Minha Lista de Tarefas" à minha lista em uma tag
+const header = document.querySelector('#header');
+
+const title = document.createElement('h2');
+title.innerText = 'Minha Lista de Tarefas';
+
+header.appendChild(title);
+
+// Adicionar um pequeno parágrafo abaixo do título com id=funcionamento
+const main = document.querySelector('#main');
+
+const paragraph = document.createElement('p');
+paragraph.id = 'funcionamento';
+paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
+main.appendChild(paragraph);
+
+/* Adicione um input com o id="texto-tarefa" onde a pessoa usuária
+poderá digitar o nome do item que deseja adicionar à lista */
+ 
+
+// Adicionar uma lista ordenada de tarefas com o id="lista-tarefas"
+const ol = document.createElement('ol');
