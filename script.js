c703ad52c81470d54aa04163cf5d0703f3073b02
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-06-08 10:19:41 -0300

    13 finalizado

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let ol = document.querySelector('#lista-tarefas');
 let tarefa = document.querySelector('#criar-tarefa');
 let apaga = document.querySelector('#apaga-tudo');
-let finalizados = document.querySelector('#remover-finalizados')
+let finalizados = document.querySelector('#remover-finalizados');
+let salvarTarefa = document.querySelector('#salvar-tarefas');
+let moverCima = document.querySelector('#mover-cima');
+let moverBaixo = document.querySelector('#mover-baixo');
 
 
 function criarTarefa(){
@@ -54,5 +57,37 @@ function removerFinalizados() {
 }
 
 finalizados.addEventListener('click', removerFinalizados)
+
+
+const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'));
+
+function salvarLista() {
+  let lista = document.querySelectorAll('#lista-tarefas li')
+  console.log(lista)
+  localStorage.setItem('lista-tarefas', JSON.stringify(tarefas));
+}
+
+salvarTarefa.addEventListener('click', salvarLista)
+
+
+function moveup() {
+  let element = document.querySelector('.selected');
+  if(element.previousElementSibling) {
+    element.parentNode.insertBefore(element, element.previousElementSibling);
+  }
+}
+
+function movedown() {
+  let element = document.querySelector('.selected');
+  if(element.nextElementSibling) {
+    element.parentNode.insertBefore(element.nextElementSibling, element);
+  }
+}
+
+moverCima.addEventListener('click', moveup)
+moverBaixo.addEventListener('click', movedown)
+
+
+
 
 
