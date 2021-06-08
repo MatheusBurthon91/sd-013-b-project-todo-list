f29da8f89a014fe58be4a8b1b4717b9836ed9dca
Gabriel <gabriel.mattar@uol.com.br>
2021-06-08 13:38:28 -0300

    Até exercicio 8

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ function criaAtividade() {
 }
 adicionar.addEventListener('click', criaAtividade);
 
-//Exercicio 7
+/*//Exercicio 7
 function marcarAtividade(event) {
   let itemSelecionado = event.target;
   itemSelecionado.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+listaTarefa.addEventListener('click', marcarAtividade);
+*/
+
+//Exercicio 8
+function marcarAtividade(event) {
+  let linhaAtividade = document.querySelectorAll('li');
+  let itemSelecionado = event.target;
+  for (let index = 0; index < linhaAtividade.length; index += 1) {
+    if (linhaAtividade[index] === itemSelecionado) {
+      itemSelecionado.classList.add('selected');
+    } else {
+      linhaAtividade[index].classList.remove('selected');
+    }
+  }
 }
 listaTarefa.addEventListener('click', marcarAtividade);
