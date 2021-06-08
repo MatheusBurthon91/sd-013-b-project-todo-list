db59962430f77186d4b2e64c2ab4c72cb6e0906e
Gabriel <gabriel.mattar@uol.com.br>
2021-06-08 13:36:32 -0300

    Até exercicio 7

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ let adicionar = document.getElementById('criar-tarefa');
 
 //Exercicio 5
 function criaAtividade() {
-let criaLinhaAtividade = document.createElement('li');
-let atividade = textoAtividade.value;
-criaLinhaAtividade.innerText = atividade;
-textoAtividade.value = '';
-criaLinhaAtividade.addEventListener('click', marcarAtividade);
-criaLinhaAtividade.addEventListener('dblclick', riscarAtividade);
-listaTarefa.appendChild(criaLinhaAtividade);
+  let criaLinhaAtividade = document.createElement('li');
+  let atividade = textoAtividade.value;
+  criaLinhaAtividade.innerText = atividade;
+  textoAtividade.value = '';
+  criaLinhaAtividade.addEventListener('click', marcarAtividade);
+  criaLinhaAtividade.addEventListener('dblclick', riscarAtividade);
+  listaTarefa.appendChild(criaLinhaAtividade);
 }
 adicionar.addEventListener('click', criaAtividade);
+
+//Exercicio 7
+function marcarAtividade(event) {
+  let itemSelecionado = event.target;
+  itemSelecionado.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+listaTarefa.addEventListener('click', marcarAtividade);
