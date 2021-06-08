a60d68d1715d031554d598dfbcbe6dbd382f22de
José Breno <brenojbs@hotmail.com>
2021-06-08 12:17:51 -0300

    desafio 8 completo?

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ document.querySelector('#criar-tarefa').addEventListener('click', function () {
   //Consegui chegar a esta solução usando a ideia do César Sousa nesta pagina  https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
   document.querySelector('#texto-tarefa').value = '';
 
-  novaT.addEventListener('click', function selecionar() {
-    novaT.classList.toggle('selected')
-    })  
+  novaT.addEventListener('click', selecionar)  
 })
+// Com a ajuda do Pedro e do Matheus eu consegui chegar a esta conclusão. O Ricardo me ajudou também em abrir meus olhos para ler novamente o event.target. Eu fui relutante em pedir ajuda a outra pessoa pois eu sinto que estou atrapalhando muito eles e acabo ficando travado. Hoje foi um passo mais próximo da liberdade.
+function selecionar(event) {
+  console.log(event.target)
+    const classe = document.querySelector('.selected') 
+    if (classe != null) {
+      classe.classList.remove('selected')
+    }
+    event.target.classList.add('selected')
+}
 
-
-// Complicado Preciso retirar uma classe
   
