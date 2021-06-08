e14bff8dcde53ca93a2d34408d741351c0a642fa
mateusparr <mateus.dji@icloud.com>
2021-06-08 00:00:15 -0300

    requisito 8 done

--- script.js
+++ script.js
@@ -3,18 +3,25 @@
 const list = document.querySelector('#lista-tarefas');
 const addTask = document.querySelector('#criar-tarefa');
 
+// eslint-disable-next-line max-lines-per-function
 function createTask() {
   const addLine = document.createElement('li');
-  addLine.className = 'item';
   addLine.innerText = document.querySelector('#texto-tarefa').value;
   list.appendChild(addLine);
   document.getElementById('texto-tarefa').value = '';
+
   addLine.addEventListener('click', (event) => {
     const selecionado = document.querySelector('.li-selected');
     if (selecionado !== null) {
       selecionado.classList.remove('li-selected');
     }
     event.target.classList.add('li-selected');
+  });
+  addLine.addEventListener('dblclick', (event) => {
+    event.target.classList.add('completed');
+    addLine.addEventListener('dblclick', () => {
+      addLine.classList.remove('completed');
+    });
   });
 }
 
