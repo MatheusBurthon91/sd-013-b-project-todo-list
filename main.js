6032c9eb4c8702471481f69544e73f7ea454ac58
Guilherme Viana <guilhermehviana01@gmail.com>
2021-06-08 10:33:04 -0300

    Adiciona botão que cria novos elementos à lista de tarefa.

--- main.js
+++ main.js
@@ -20,5 +20,27 @@ poderá digitar o nome do item que deseja adicionar à lista */
 
 // Adicionar uma lista ordenada de tarefas com o id="lista-tarefas"
 const body = document.querySelector('body');
+const input = document.querySelector('#texto-tarefa');
+
+main.appendChild(input);
 
 const ol = document.createElement('ol');
+ol.id = 'lista-tarefas';
+body.appendChild(ol);
+
+// Adicionar um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+//Criar botão
+const taskButton = document.createElement('button');
+
+taskButton.id = 'criar-tarefas';
+taskButton.innerText = 'Add';
+main.appendChild(taskButton);
+
+// ao clicar no botão, cria-se um novo item à lista
+function addTask() {
+    let taskItem = document.createElement('li');
+    taskItem.innerText = input.value;
+    ol.appendChild(taskItem);
+}
+
+taskButton.addEventListener('click', addTask);
