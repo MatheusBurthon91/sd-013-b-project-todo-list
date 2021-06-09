68c97860bc7f51c58e5c215e7229d046a8f9a20c
Henrique Cursino <henriquecursino46@gmail.com>
2021-06-08 21:26:16 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// Nesse Requisito eu tive ajuda do Lopes e do Pedro!
+function selected(event){
+    const allLi = document.getElementsByClassName('selected')[0];
+    if (allLi) {
+        allLi.classList.remove('selected');
+    }
+        event.target.classList.add('selected');
+}
+
 const button = document.querySelector('#criar-tarefa');
 function adicionar() {
     const text = document.getElementById('texto-tarefa').value;
     const list = document.getElementById('lista-tarefas');
     const newEl = document.createElement('li');
+    newEl.addEventListener('click', selected);
     document.querySelector('#texto-tarefa').value = null;
     newEl.innerText = text;
     list.appendChild(newEl);
