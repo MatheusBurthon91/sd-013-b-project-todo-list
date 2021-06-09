274f69e5f0bdd51c40ddd9df0e8b2ed510df6dbb
larymanzo <lamosinha@hotmail.com>
2021-06-09 19:26:13 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const list = document.getElementById('lista-tarefas');
-const button = document.getElementById('criar-tarefa')
+const button = document.getElementById('criar-tarefa');
 
 // criando a lista
 function createList() {
   const text = document.getElementById('texto-tarefa');
   const listOrd = document.createElement('li');
 
-  if (text.value !== ''){
+  //  adicionando o texto na li
+  if (text.value !== '') {
     listOrd.innerText = text.value;
+    list.appendChild(listOrd);
+    listOrd.style.backgroundColor = 'white'
+  } else {
+
   }
-  list.appendChild(listOrd);
-  text.value = ''
+  text.value = '';
 }
 
-//criando o botão que vai adicionar a list
+//  criando o botão que vai adicionar a list
 button.addEventListener('click', createList);
+
+// pega os filhos da ol
+const section1 = document.getElementById('lista-tarefas');
+
+function setColor(event) {
+  const clickColor = event.target;
+  clickColor.style.backgroundColor = 'rgb(128, 128, 128)'
+}
+
+  section1.addEventListener('click', setColor);
