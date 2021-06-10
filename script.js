16a7d85efc96c955bf750acadd779af9fbc3a0e7
r0ger <rogeriomaiajr13@gmail.com>
2021-06-10 11:43:46 -0300

    Requisito 9 concluido

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const buttonTaskCreated = document.querySelector('#criar-tarefa');
 const txtInput = document.querySelector('#texto-tarefa');
 const tasksList = document.querySelector('#lista-tarefas');
+const section = document.querySelector('#box-section');
 
 function createTask(task, classOfTask) {
   const listItem = document.createElement('li');
   listItem.innerText = task;
   listItem.className = classOfTask;
   tasksList.appendChild(listItem);
-  txtInput.value = ''; // call the function to clear the input
+  txtInput.value = '';
 }
 
 function createTaskBtn() {
@@ -16,18 +17,35 @@ function createTaskBtn() {
   createTask(task, classOfTask);
 }
 
+function removeButton() {
+  const button = document.createElement('button');
+  button.id = 'apaga-tudo';
+  button.innerHTML = 'Apaga tarefas da lista';
+  section.appendChild(button);
+}
+
+removeButton();
+
 buttonTaskCreated.addEventListener('click', createTaskBtn);
 
 tasksList.addEventListener('click', (event) => {
-  const assistant = event;
+  const auxiliar = event;
+  console.log(auxiliar);
   const classToRemove = document.querySelector('.selected');
   if (classToRemove === null) {
-    assistant.target.classList.add('selected');
-    assistant.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    auxiliar.target.classList.add('selected');
   } else {
     classToRemove.classList.remove('selected');
-    classToRemove.style.backgroundColor = 'white';
-    assistant.target.classList.add('selected');
-    assistant.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    auxiliar.target.classList.add('selected');
+  }
+});
+
+tasksList.addEventListener('dblclick', (event) => {
+  const auxiliar = event;
+  console.log(event);
+  if (auxiliar.target.classList.contains('completed')) {
+    auxiliar.target.classList.remove('completed');
+  } else {
+    auxiliar.target.classList.add('completed');
   }
 });
