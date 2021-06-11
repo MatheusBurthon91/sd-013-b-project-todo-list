3334f1bf5ddf7add392ee3995889f195402abe75
Julio Cesar <juliosinu@gmail.com>
2021-06-10 23:19:48 -0300

    requisito 8 revisado

--- script.js
+++ script.js
@@ -8,16 +8,29 @@ function mudarBG (li) {
     const rowSelected = event.target;
     const rowSelectedPrevious = document.querySelector('.selected');
     console.log(rowSelectedPrevious);
-    rowSelectedPrevious.style.backgroundColor = 'white';
     rowSelectedPrevious.classList.remove('selected');
     rowSelected.classList.add('selected');
     const linhaSelect = document.querySelector('.selected');
+    if (rowSelected !== rowSelectedPrevious){
+      rowSelectedPrevious.style.backgroundColor = 'white';
+    }
     if (linhaSelect.style.backgroundColor === 'rgb(128, 128, 128)'){
-      linhaSelect.style.backgroundColor = 'white'; 
+      linhaSelect.style.backgroundColor = 'white';
     }else {
       linhaSelect.style.backgroundColor = 'rgb(128, 128, 128)';
     } 
-  })
+  });
+}
+
+function mudarCompleted (la) {
+  la.addEventListener('dblclick', (event) => {
+    const changeComplete = event.target;
+    if (changeComplete.className === 'completed'){
+      event.target.classList.remove('completed');
+    }else {
+      event.target.classList.add('completed');
+    }     
+  });
 }
 
 criarTarefa.addEventListener('click', () => {
@@ -28,5 +41,6 @@ criarTarefa.addEventListener('click', () => {
   input.value = '';
   const linhaselect = document.getElementsByTagName('li')[0];
   linhaselect.classList.add('selected');
+  mudarCompleted(listaTarefa);
 });
 
