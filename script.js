8bbc22aae55fdf194db5b30a7354e560aa81d33c
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-06-11 14:19:47 -0300

    retomando2

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let input = document.querySelector('#texto-tarefa')
 let button = document.querySelector('#criar-tarefa')
 
 function Makelist() {
-  let valor = input.value
+  let text = input.value
   let li = document.createElement('li')
-  li.innerHTML = valor
+  li.innerHTML = text
   list.appendChild(li)
   input.value = ''
 }
@@ -13,18 +13,18 @@ button.addEventListener('click', Makelist)
 
 function changeColor(event) {
   remove()
-  event.target.classList.add('select')
+  event.target.classList.add('selected')
 }
-lista.addEventListener('click', changeColor)
+list.addEventListener('click', changeColor)
 
 function remove() {
-  let removeClass = document.querySelectorAll('.select')[0]
+  let removeClass = document.querySelectorAll('.selected')[0]
   if (removeClass) {
-    removeClass.classList.remove('select')
+    removeClass.classList.remove('selected')
   }
 }
 
 function riscar(event) {
   event.target.classList.toggle('completed')
 }
-lista.addEventListener('dblclick', riscar)
+list.addEventListener('dblclick', riscar)
