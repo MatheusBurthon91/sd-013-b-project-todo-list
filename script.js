9e8b127fe9ac1ee98b372e4e6aa7c35c4eb81463
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-06-11 19:33:33 -0300

    Challenge development updated

--- script.js
+++ script.js
@@ -1,60 +1,90 @@
 const tarefaInput = document.querySelector('#texto-tarefa');
-const listaTarefas = document.querySelector('#lista-tarefas');
-const tarefas = document.querySelectorAll('.item');
 const tarefaBtn = document.querySelector('#criar-tarefa');
-const apagarTudoBtn = document.querySelector('#apagar-tudo');
+const apagaTudoBtn = document.querySelector('#apaga-tudo');
+const salvaTarefas = document.querySelector('#salvar-tarefas');
+const removeSelecionado = document.querySelector('#remover-selecionado');
+const removeFinalizados = document.querySelector('#remover-finalizados');
+let listaTarefas;
+let tarefas;
 
-function adicionarTarefa() {
-  tarefaBtn.addEventListener('click', () => {
-    const valorDigitado = tarefaInput.value;
-    if (valorDigitado !== '') {
+function selecionarTarefa(event) {
+  tarefas = document.querySelectorAll('.item');
+  for (let index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+function completarTarefa(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+function carregarStorage() {
+  const itensSalvos = JSON.parse(localStorage.getItem('Itens ToDo'));
+  const classesSalvas = JSON.parse(localStorage.getItem('ToDo Classes'));
+  if (itensSalvos) {
+    listaTarefas = document.querySelector('#lista-tarefas');
+    for (let index = 0; index < itensSalvos.length; index += 1) {
       const item = document.createElement('li');
-      item.classList.add('item');
-      item.innerText = valorDigitado;
+      item.classList.add(classesSalvas[index]);
+      item.innerText = itensSalvos[index];
+      item.addEventListener('click', selecionarTarefa);
+      item.addEventListener('dblclick', completarTarefa);
       listaTarefas.appendChild(item);
-      tarefaInput.value = '';
     }
-  });
+  }
 }
 
-function selecionarTarefa() {
-  tarefas.forEach((element) => {
-    element.addEventListener('click', (e) => {
-      for (let index = 0; index < tarefas.length; index += 1) {
-        tarefas[index].classList.remove('selected');
-      }
-      e.target.classList.add('selected');
-    });
-  });
-}
+tarefaBtn.onclick = () => {
+  const valorDigitado = tarefaInput.value;
+  if (valorDigitado !== '') {
+    listaTarefas = document.querySelector('#lista-tarefas');
+    const item = document.createElement('li');
+    item.classList.add('item');
+    item.innerText = valorDigitado;
+    item.addEventListener('click', selecionarTarefa);
+    item.addEventListener('dblclick', completarTarefa);
+    listaTarefas.appendChild(item);
+    tarefaInput.value = '';
+  }
+};
 
-function completarTarefa() {
-  tarefas.forEach((element) => {
-    element.addEventListener('dblclick', (e) => {
-      e.target.classList.add('completed');
-    });
-  });
-}
+salvaTarefas.onclick = () => {
+  const itens = document.querySelectorAll('.item');
+  const listaItens = [];
+  const listaClasses = [];
+  for (let index = 0; index < itens.length; index += 1) {
+    listaItens.push(itens[index].innerHTML);
+    listaClasses.push(itens[index].classList);
+    localStorage.setItem('Itens ToDo', JSON.stringify(listaItens));
+    localStorage.setItem('ToDo Classes', JSON.stringify(listaClasses));
+  }
+};
 
-function descompletarTarefa() {
-  tarefas.forEach((element) => {
-    element.addEventListener('dblclick', (e) => {
-      if (e.target.classList.contains('completed')) {
-        e.target.classList.remove('completed');
-      }
-    });
-  });
-}
+removeSelecionado.onclick = () => {
+  const tarefaSelecionada = document.querySelector('.selected');
+  tarefaSelecionada.remove();
+};
 
-function apagarTudo() {
-  apagarTudoBtn.addEventListener('click', () => {
-    for (let index = 0; index < tarefas.length; index += 1) {
-      tarefas[index].remove();
-    }
+removeFinalizados.onclick = () => {
+  const tarefasFinalizadas = document.querySelectorAll('.completed');
+  tarefasFinalizadas.forEach((element) => {
+    element.remove();
   });
-}
-adicionarTarefa();
-selecionarTarefa();
-completarTarefa();
-descompletarTarefa();
-apagarTudo();
+};
+
+apagaTudoBtn.onclick = () => {
+  tarefas = document.querySelectorAll('.item');
+  for (let index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].remove();
+  }
+  localStorage.clear();
+};
+
+window.onload = () => {
+  carregarStorage();
+};
