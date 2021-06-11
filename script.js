7eecc2754266e108f6ac122e7f52c1c1f0529262
João Victor Rodrigues Del Papa <joaodelpapa1@gmail.com>
2021-06-11 19:46:52 -0300

    Requisitos ate o 11, trocando as lets pelos consts

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-let taskInput = document.getElementById('texto-tarefa');
-let list = document.createElement('ol');
-let taskButton = document.createElement('button');
-let listItens = document.getElementsByClassName('list-item');
-let clearCompletedButton = document.createElement('button');
-let clearButton = document.createElement('button');
+const taskInput = document.getElementById('texto-tarefa');
+const list = document.createElement('ol');
+const taskButton = document.createElement('button');
+const listItens = document.getElementsByClassName('list-item');
+const clearCompletedButton = document.createElement('button');
+const clearButton = document.createElement('button');
 
-function changeBackgroundColor(event) {
-    for (let listElements of listItens) {
-        if (listElements.className.includes('selected')) {
-            listElements.classList.remove('selected');
-            listElements.style.backgroundColor = 'white';
-        }
+function changeBackgrondColor(event) {
+  for (const listElements of listItens) {
+    if (listElements.className.includes('selected')) {
+      listElements.classList.remove('selected');
+      listElements.style.backgroundColor = 'white';
     }
-    event.target.className += ' selected';
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+  event.target.className += ' selected';
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
 function changeCompleted(event) {
-    if (event.target.className.includes('completed')) {
-        event.target.classList.remove('completed');
-    } else {
-        event.target.className += ' completed';
-    }
+  if (event.target.className.includes('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.className += ' completed';
+  }
 }
 
 function updateListItens() {
-    for (let listElements of listItens) {
-        listElements.addEventListener('click', changeBackgroundColor);
-        listElements.addEventListener('dbclick', changeCompleted);
-    }
+  for (const listElements of listItens) {
+    listElements.addEventListener('click', changeBackgrondColor);
+    listElements.addEventListener('dblclick', changeCompleted);
+  }
 }
 
 function addTask() {
-    let listItem = document.createElement('li')
-    listItem.className = 'list-item';
-    listItem.innerText = taskInput.value;
-    list.appendChild(listItem);
-    taskInput.value = '';
-    updateListItens();
+  const listItem = document.createElement('li');
+  listItem.className = 'list-item';
+  listItem.innerText = taskInput.value;
+  list.appendChild(listItem);
+  taskInput.value = '';
+  updateListItens();
 }
 
 function clearList() {
-    while (list.hasChildNodes()) {
-        list.removeChild(list.firstChild);
-    }
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
 }
 
 function clearCompleted() {
-    let completedItens = document.getElementsByClassName('completed');
-    while (completedItens.length > 0) {
-        completedItens[0].remove();
-    }
+  const completedItens = document.getElementsByClassName('completed');
+  while (completedItens.length > 0) {
+    completedItens[0].remove();
+  }
 }
 
 list.id = 'lista-tarefas';
@@ -66,6 +66,6 @@ clearButton.innerText = 'Limpar lista';
 document.body.appendChild(clearButton);
 clearButton.addEventListener('click', clearList);
 clearCompletedButton.id = 'remover-finalizados';
-clearCompletedButton.innerText = 'Remove itens finalizados';
+clearCompletedButton.innerText = 'Remover itens finalizados';
 document.body.appendChild(clearCompletedButton);
 clearCompletedButton.addEventListener('click', clearCompleted);
